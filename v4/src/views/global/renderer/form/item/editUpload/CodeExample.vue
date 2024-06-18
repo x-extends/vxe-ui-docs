f6a6895dfe79b4dae8c811c1a6fe905c97cc2a0c
xuliangzhan <xlz26296x@163.com>
2024-06-18 12:32:44 +0800

    update docs

--- v4/src/views/global/renderer/form/item/editUpload/CodeExample.vue
+++ v4/src/views/global/renderer/form/item/editUpload/CodeExample.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <CodeLight path="global/renderer/form/item/editUpload/Demo1" :extraImports="['./demo1.table.renderer.tsx']">
+    <CodeLight path="global/renderer/form/item/editUpload/Demo1" :extraImports="['./demo1.form.renderer.tsx']">
       <template #tip>
         <vxe-tip status="primary" title="实现一个附件上传">基础使用</vxe-tip>
       </template>
     </CodeLight>
 
-    <CodeLight path="global/renderer/form/item/editUpload/Demo2" :extraImports="['./demo1.table.renderer.tsx']">
+    <CodeLight path="global/renderer/form/item/editUpload/Demo2" :extraImports="['./demo1.form.renderer.tsx']">
       <template #tip>
         <vxe-tip status="primary" title="配置式使用"></vxe-tip>
       </template>
