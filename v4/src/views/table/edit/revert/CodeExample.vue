1056a5ce4ba2c027bbaa419ed7de55c819418aba
xuliangzhan <xlz26296x@163.com>
2024-06-20 16:48:42 +0800

    update docs

--- v4/src/views/table/edit/revert/CodeExample.vue
+++ v4/src/views/table/edit/revert/CodeExample.vue
@@ -2,7 +2,7 @@
   <div>
     <CodeLight path="table/edit/revert/Demo1">
       <template #tip>
-        <vxe-tip status="primary">设置 <table-api-link prop="keep-source"/> 开启保持原始值状态，通过调用 <table-api-link prop="revertData"/> 还原数据</vxe-tip>
+        <vxe-tip status="primary">设置 <ApiLink name="table" prop="keep-source"/> 开启保持原始值状态，通过调用 <ApiLink name="table" prop="revertData"/> 还原数据</vxe-tip>
       </template>
     </CodeLight>
   </div>
