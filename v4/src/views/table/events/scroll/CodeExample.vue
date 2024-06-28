17c353ba31f21191e575ad0ce5153deb5d92094e
xuliangzhan <xlz26296x@163.com>
2024-06-28 12:57:05 +0800

    update docs

--- v4/src/views/table/events/scroll/CodeExample.vue
+++ v4/src/views/table/events/scroll/CodeExample.vue
@@ -3,7 +3,7 @@
     <CodeLight path="table/events/scroll/Demo1">
       <template #tip>
         <vxe-tip status="primary" title="滚动事件">
-          可以监听滚动条事件
+          可以通过 <ApiLink name="table" prop="scroll"/> 监听滚动条事件
         </vxe-tip>
       </template>
     </CodeLight>
