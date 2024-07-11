e7031dae36bb18d4c39a2eee971e0cfb0122b9d1
xuliangzhan <xlz26296x@163.com>
2024-07-11 20:02:50 +0800

    update docs

--- v4/src/views/treeSelect/icon/Demo1.vue
+++ v4/src/views/treeSelect/icon/Demo1.vue
@@ -11,45 +11,46 @@
 
 <script lang="ts" setup>
 import { ref } from 'vue'
-import { VxeTreePropTypes } from 'vxe-pc-ui'
+import { VxeTreeSelectPropTypes } from 'vxe-pc-ui'
 
 const val1 = ref()
-const treeList = ref<VxeTreePropTypes.Options>([
-  { label: '节点2', value: 2 },
+
+const treeList = ref<VxeTreeSelectPropTypes.Options>([
+  { label: '节点2', value: '2' },
   {
     label: '节点3',
-    value: 3,
+    value: '3',
     children: [
-      { label: '节点3-1', value: 31 },
-      { label: '节点3-2', value: 32 },
+      { label: '节点3-1', value: '31' },
+      { label: '节点3-2', value: '32' },
       {
         label: '节点3-3',
-        value: 33,
+        value: '33',
         children: [
-          { label: '节点3-3-1', value: 331 },
-          { label: '节点3-3-2', value: 332 },
-          { label: '节点3-3-3', value: 333 }
+          { label: '节点3-3-1', value: '331' },
+          { label: '节点3-3-2', value: '332' },
+          { label: '节点3-3-3', value: '333' }
         ]
       },
-      { label: '节点3-4', value: 34 }
+      { label: '节点3-4', value: '34' }
     ]
   },
   {
     label: '节点4',
-    value: 4,
+    value: '4',
     children: [
-      { label: '节点4-1', value: 41 },
-      { label: '节点4-2', value: 42 },
+      { label: '节点4-1', value: '41' },
+      { label: '节点4-2', value: '42' },
       {
         label: '节点4-3',
-        value: 43,
+        value: '43',
         children: [
-          { label: '节点4-3-1', value: 431 },
-          { label: '节点4-3-2', value: 432 }
+          { label: '节点4-3-1', value: '431' },
+          { label: '节点4-3-2', value: '432' }
         ]
       }
     ]
   },
-  { label: '节点5', value: 5 }
+  { label: '节点5', value: '5' }
 ])
 </script>
