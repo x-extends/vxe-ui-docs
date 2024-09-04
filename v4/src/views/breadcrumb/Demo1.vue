f6b168f5a8bd61424ebc4c96f6eacbed8efaa286
xuliangzhan <xlz26296x@163.com>
2024-09-04 12:00:04 +0800

    update docs

--- v4/src/views/breadcrumb/Demo1.vue
+++ v4/src/views/breadcrumb/Demo1.vue
@@ -2,9 +2,9 @@
   <div>
     <p>
       <vxe-breadcrumb>
-        <vxe-breadcrumb-item>首页</vxe-breadcrumb-item>
-        <vxe-breadcrumb-item>系统设置</vxe-breadcrumb-item>
-        <vxe-breadcrumb-item>菜单设置</vxe-breadcrumb-item>
+        <vxe-breadcrumb-item title="首页"></vxe-breadcrumb-item>
+        <vxe-breadcrumb-item title="系统设置"></vxe-breadcrumb-item>
+        <vxe-breadcrumb-item title="菜单设置"></vxe-breadcrumb-item>
       </vxe-breadcrumb>
     </p>
   </div>
