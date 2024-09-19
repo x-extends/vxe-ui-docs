a75189c8ecb19ed2fd167c279a8d56c658545719
xuliangzhan <xlz26296x@163.com>
2024-09-19 16:31:05 +0800

    update docs

--- v4/src/views/grid/base/columns/CodeExample.vue
+++ v4/src/views/grid/base/columns/CodeExample.vue
@@ -3,7 +3,7 @@
     <CodeLight path="grid/base/columns/Demo1">
       <template #tip>
         <vxe-tip status="primary" title="更新列配置">
-          列和数据都是双向同步，改变对应的属性即可更新
+          表格列 <ApiLink name="grid" prop="columns"/> 和数据 <ApiLink name="grid" prop="data"/> 都是双向同步，改变对应的属性即可更新
         </vxe-tip>
       </template>
     </CodeLight>
