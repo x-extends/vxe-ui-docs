48ea269674e6a59ed9e31bf615320f883f2cedf7
xuliangzhan <xlz26296x@163.com>
2024-10-03 17:43:57 +0800

    update docs

--- v4/src/views/table/editTemplate/vxe-pulldown/Demo1.vue
+++ v4/src/views/table/editTemplate/vxe-pulldown/Demo1.vue
@@ -43,6 +43,11 @@ interface RowVO {
 const tableData = ref<RowVO[]>([
   { id: 10001, name: 'Test1', role: 'Develop', userName: 'test6' },
   { id: 10002, name: 'Test2', role: 'Test', userName: 'test1' },
-  { id: 10003, name: 'Test3', role: 'PM', userName: 'test5' }
+  { id: 10003, name: 'Test3', role: 'PM', userName: 'test5' },
+  { id: 10004, name: 'Test4', role: 'Test', userName: 'test5' },
+  { id: 10005, name: 'Test5', role: 'Develop', userName: 'test2' },
+  { id: 10006, name: 'Test6', role: 'Develop', userName: 'test1' },
+  { id: 10007, name: 'Test7', role: 'PM', userName: 'test7' },
+  { id: 10008, name: 'Test8', role: 'Develop', userName: 'test5' }
 ])
 </script>
