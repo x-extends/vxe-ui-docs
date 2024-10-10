7ed5ee90214b2a938b171344e52e767f90f99c1d
xuliangzhan <xlz26296x@163.com>
2024-10-10 18:38:24 +0800

    update docs

--- other4/src/views/start/theme/CodeExample.vue
+++ other4/src/views/start/theme/CodeExample.vue
@@ -2,8 +2,8 @@
   <div>
     <CodeLight>
       <template #tip>
-        <vxe-tip status="primary" title="自定义主题">
-          默认支持 light 和 dark 两种主题样式，通过调用 VxeUI.setTheme('light') 方法修改组件主题样式
+        <vxe-tip status="primary" title="内置主题">
+          默认支持 light 和 dark 两种主题样式，通过调用 VxeUI.setTheme('light') 方式修改组件主题样式
         </vxe-tip>
       </template>
 
@@ -17,6 +17,25 @@
 
             // 切换为暗黑主题
             VxeUI.setTheme('dark')
+          </pre-code>
+        </pre>
+      </template>
+    </CodeLight>
+
+    <CodeLight>
+      <template #tip>
+        <vxe-tip status="primary" title="自定义主题">
+          通过修改 css 变量（<vxe-link href="https://gitee.com/x-extends/vxe-pc-ui/blob/main/styles/theme/light.scss" status="primary">查看全部变量</vxe-link>）方式修改组件主题样式，给 html 加个 class="my-theme"，示例：
+        </vxe-tip>
+      </template>
+
+      <template #use>
+        <pre>
+          <pre-code class="css">
+            html.my-theme {
+              // 主题色
+              --vxe-ui-font-primary-color: #44bf16;
+            }
           </pre-code>
         </pre>
       </template>
