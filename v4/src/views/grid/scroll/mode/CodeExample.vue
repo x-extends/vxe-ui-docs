3dc7d998e05e0ccf5630039bc21a5e1786c87d50
xuliangzhan <xlz26296x@163.com>
2024-10-16 09:22:45 +0800

    update docs

--- v4/src/views/grid/scroll/mode/CodeExample.vue
+++ v4/src/views/grid/scroll/mode/CodeExample.vue
@@ -5,7 +5,7 @@
         <vxe-tip status="primary" title="优化模式">
           支持原生和优化两种模式，可以通过 <ApiLink name="grid" prop="scroll-y"/>.mode='wheel' 设置为优化模式
         </vxe-tip>
-        <vxe-tip status="error">
+        <vxe-tip status="success">
           优化模式可以解决虚拟滚动的渲染空白期
         </vxe-tip>
       </template>
