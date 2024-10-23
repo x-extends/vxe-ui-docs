44846e2e14b58209a583d587200059a7f126df20
xuliangzhan <xlz26296x@163.com>
2024-10-23 16:51:08 +0800

    update docs

--- v3/src/views/checkbox/indeterminate/Demo1.vue
+++ v3/src/views/checkbox/indeterminate/Demo1.vue
@@ -1,9 +1,7 @@
 <template>
   <div>
-    <vxe-checkbox v-model="val1" content="默认尺寸" indeterminate></vxe-checkbox>
-    <vxe-checkbox v-model="val2" size="medium" content="中等尺寸" indeterminate></vxe-checkbox>
-    <vxe-checkbox v-model="val3" size="small" content="小型尺寸" indeterminate></vxe-checkbox>
-    <vxe-checkbox v-model="val4" size="mini" content="超小尺寸" indeterminate></vxe-checkbox>
+    <vxe-checkbox v-model="val1" content="已选中" indeterminate></vxe-checkbox>
+    <vxe-checkbox v-model="val2" content="未选中" indeterminate></vxe-checkbox>
   </div>
 </template>
 
@@ -13,10 +11,8 @@ import Vue from 'vue'
 export default Vue.extend({
   data () {
     return {
-      val1: false,
-      val2: false,
-      val3: false,
-      val4: false
+      val1: true,
+      val2: false
     }
   }
 })
