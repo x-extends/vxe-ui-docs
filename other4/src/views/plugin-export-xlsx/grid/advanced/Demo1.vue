f6e2b51dcde3b2734119a0589c421535734f2fb3
xuliangzhan <xlz26296x@163.com>
2024-11-01 16:18:39 +0800

    update docs

--- other4/src/views/plugin-export-xlsx/grid/advanced/Demo1.vue
+++ other4/src/views/plugin-export-xlsx/grid/advanced/Demo1.vue
@@ -23,6 +23,10 @@ const gridRef = ref<VxeGridInstance>()
 const gridOptions = reactive<VxeGridProps<RowVO>>({
   border: true,
   showFooter: true,
+  mergeCells: [
+    { row: 0, col: 2, rowspan: 2, colspan: 2 },
+    { row: 2, col: 1, rowspan: 3, colspan: 2 }
+  ],
   exportConfig: {
     type: 'xlsx'
   },
@@ -42,7 +46,9 @@ const gridOptions = reactive<VxeGridProps<RowVO>>({
     { id: 10001, name: '张三', role: 'Develop', sex: 'Man', age: 28, address: 'test abc' },
     { id: 10002, name: '李四', role: 'Test', sex: 'Women', age: 22, address: '广东省' },
     { id: 10003, name: '王五', role: 'PM', sex: 'Man', age: 32, address: '上海' },
-    { id: 10004, name: '老六', role: 'Designer', sex: 'Women', age: 24, address: 'Shanghai' }
+    { id: 10004, name: '老六', role: 'Designer', sex: 'Women', age: 24, address: 'Shanghai' },
+    { id: 10005, name: '小七', role: 'Designer', sex: 'Man', age: 37, address: '广东省' },
+    { id: 10006, name: '王八', role: 'Designer', sex: 'Man', age: 39, address: 'Shanghai' }
   ],
   footerData: [
     { seq: '合计', name: '12人', no1: 356 }
--- other4/src/views/plugin-export-xlsx/table/advanced/Demo1.vue
+++ other4/src/views/plugin-export-xlsx/table/advanced/Demo1.vue
@@ -7,6 +7,7 @@
       ref="tableRef"
       :export-config="{type: 'xlsx'}"
       :data="tableData"
+      :merge-cells="mergeCells"
       :footer-data="footerData">
       <vxe-column field="seq" type="seq" width="70"></vxe-column>
       <vxe-colgroup title="分组1">
@@ -38,7 +39,14 @@ const tableData = ref<RowVO[]>([
   { id: 10001, name: '张三', role: 'Develop', sex: 'Man', age: 28, address: 'test abc' },
   { id: 10002, name: '李四', role: 'Test', sex: 'Women', age: 22, address: '广东省' },
   { id: 10003, name: '王五', role: 'PM', sex: 'Man', age: 32, address: '上海' },
-  { id: 10004, name: '老六', role: 'Designer', sex: 'Women', age: 24, address: 'Shanghai' }
+  { id: 10004, name: '老六', role: 'Designer', sex: 'Women', age: 24, address: 'Shanghai' },
+  { id: 10005, name: '小七', role: 'Designer', sex: 'Man', age: 37, address: '广东省' },
+  { id: 10006, name: '王八', role: 'Designer', sex: 'Man', age: 39, address: 'Shanghai' }
+])
+
+const mergeCells = ref<VxeTablePropTypes.MergeCells>([
+  { row: 0, col: 2, rowspan: 2, colspan: 2 },
+  { row: 2, col: 1, rowspan: 3, colspan: 2 }
 ])
 
 const footerData = ref<VxeTablePropTypes.FooterData>([
