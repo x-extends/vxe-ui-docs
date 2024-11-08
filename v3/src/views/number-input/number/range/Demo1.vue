1598cf72250f997ba04035e27752149101f9dea9
xuliangzhan <xlz26296x@163.com>
2024-11-08 09:19:08 +0800

    update docs

---
+++ v3/src/views/number-input/number/range/Demo1.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <vxe-number-input v-model="val1" placeholder="最小值" type="number" min="5"></vxe-number-input>
+    <vxe-number-input v-model="val2" placeholder="最大值" type="number" max="50"></vxe-number-input>
+    <vxe-number-input v-model="val3" placeholder="最小值与最大值" type="number" min="10" max="100"></vxe-number-input>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data () {
+    return {
+      val1: null,
+      val2: null,
+      val3: null
+    }
+  }
+})
+</script>
---
+++ v4/src/views/number-input/number/range/Demo1.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <vxe-number-input v-model="val1" placeholder="最小值" type="number" min="5"></vxe-number-input>
+    <vxe-number-input v-model="val2" placeholder="最大值" type="number" max="50"></vxe-number-input>
+    <vxe-number-input v-model="val3" placeholder="最小值与最大值" type="number" min="10" max="100"></vxe-number-input>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const val1 = ref(null)
+const val2 = ref(null)
+const val3 = ref(null)
+</script>
