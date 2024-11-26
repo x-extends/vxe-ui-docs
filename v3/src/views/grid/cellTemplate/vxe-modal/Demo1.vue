59cbb1f219e9bb6ffe7a9305afe5922f48882299
xuliangzhan <xlz26296x@163.com>
2024-11-26 17:18:23 +0800

    update docs

--- v3/src/views/grid/cellTemplate/vxe-modal/Demo1.vue
+++ v3/src/views/grid/cellTemplate/vxe-modal/Demo1.vue
@@ -1,27 +1,43 @@
 <template>
   <div>
     <vxe-grid v-bind="gridOptions">
-      <template #flag1_default="{ row }">
-        <vxe-switch v-model="row.flag1" disabled></vxe-switch>
-      </template>
-
-      <template #flag2_default="{ row }">
-        <vxe-switch v-model="row.flag2"></vxe-switch>
+      <template #action="{ row }">
+        <vxe-button mode="text" status="primary" @click="selectEvent(row)">关联订单</vxe-button>
       </template>
     </vxe-grid>
+
+    <vxe-modal
+      resize
+      show-footer
+      show-confirm-button
+      show-cancel-button
+      v-model="showPopup"
+      title="关联订单"
+      height="400"
+      width="800"
+      @show="showSubEvent"
+      @confirm="confirmSubEvent">
+      <vxe-grid ref="productGridRef" v-bind="productGridOptions"></vxe-grid>
+    </vxe-modal>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import type { VxeGridProps } from 'vxe-table'
+import { VxeGridProps, VxeGridInstance } from 'vxe-table'
 
 interface RowVO {
   id: number
   name: string
-  role: string
-  flag1: boolean
-  flag2: boolean
+  productId: number | null
+  productName: string
+  productPrice: number| null
+}
+
+interface ProductVO {
+  id: number
+  name: string
+  price: number
 }
 
 export default Vue.extend({
@@ -31,19 +47,71 @@ export default Vue.extend({
       showOverflow: true,
       columns: [
         { type: 'seq', width: 70 },
-        { field: 'name', title: 'Name', minWidth: 200 },
-        { field: 'flag1', title: '开关', width: 120, slots: { default: 'flag1_default' } },
-        { field: 'flag2', title: '可编辑', width: 120, slots: { default: 'flag2_default' } }
+        { field: 'name', title: '采购人员', minWidth: 200 },
+        { field: 'productName', title: '商品名称', minWidth: 200 },
+        { field: 'productPrice', title: '商品价格', width: 120 },
+        { title: '操作', width: 200, slots: { default: 'action' } }
       ],
       data: [
-        { id: 10001, name: 'Test1', role: 'Develop', flag1: false, flag2: false },
-        { id: 10002, name: 'Test2', role: 'Test', flag1: true, flag2: true },
-        { id: 10003, name: 'Test3', role: 'PM', flag1: false, flag2: false }
+        { id: 10001, productId: null, name: '张三', productName: '', productPrice: null },
+        { id: 10002, productId: null, name: '李四', productName: '', productPrice: null },
+        { id: 10003, productId: null, name: '王五', productName: '', productPrice: null }
+      ]
+    }
+
+    const productGridOptions: VxeGridProps<ProductVO> = {
+      border: true,
+      height: '100%',
+      rowConfig: {
+        keyField: 'id'
+      },
+      columns: [
+        { type: 'radio', width: 60 },
+        { field: 'name', title: '名称' },
+        { field: 'price', title: '价格' }
+      ],
+      data: [
+        { id: 1001, name: 'Vxe 企业版授权', price: 12499 },
+        { id: 1002, name: 'Vxe 高级筛选扩展', price: 1099 },
+        { id: 1003, name: 'Vxe 零代码平台', price: 16888 }
       ]
     }
 
     return {
-      gridOptions
+      gridOptions,
+      productGridOptions,
+      showPopup: false,
+      selectRow: null as RowVO | null
+    }
+  },
+  methods: {
+    selectEvent (row: RowVO) {
+      this.showPopup = true
+      this.selectRow = row
+    },
+    showSubEvent () {
+      const $productGrid = this.$refs.productGridRef as VxeGridInstance<ProductVO>
+      if ($productGrid) {
+        const row = this.selectRow
+        if (row && row.productId) {
+          const productRow = $productGrid.getRowById(row.productId)
+          $productGrid.setRadioRow(productRow)
+        } else {
+          $productGrid.clearRadioRow()
+        }
+      }
+    },
+    confirmSubEvent () {
+      const $productGrid = this.$refs.productGridRef as VxeGridInstance<ProductVO>
+      if ($productGrid) {
+        const row = this.selectRow
+        const selectProduct = $productGrid.getRadioRecord()
+        if (row && selectProduct) {
+          row.productId = selectProduct.id
+          row.productName = selectProduct.name
+          row.productPrice = selectProduct.price
+        }
+      }
     }
   }
 })
