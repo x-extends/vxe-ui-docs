b6478e19a039a1b541a03b0d3ad7178b8af7c06b
xuliangzhan <xlz26296x@163.com>
2024-11-29 12:43:40 +0800

    update docs

--- v3/src/views/table/edit/modalSelect/CodeExample.vue
+++ v3/src/views/table/edit/modalSelect/CodeExample.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <CodeLight path="table/edit/modalSelect/Demo1">
+    <CodeLight path="table/edit/modalSelect/Demo1" previewPath="/resource/docsImg/table_edit_modal_select.gif">
       <template #tip>
         <vxe-tip status="primary" title="实现弹窗选择数据">
         </vxe-tip>
--- v4/src/views/grid/edit/modalSelect/CodeExample.vue
+++ v4/src/views/grid/edit/modalSelect/CodeExample.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <CodeLight path="grid/edit/modalSelect/Demo1">
+    <CodeLight path="grid/edit/modalSelect/Demo1" previewPath="/resource/docsImg/table_edit_modal_select.gif">
       <template #tip>
         <vxe-tip status="primary" title="实现弹窗选择数据">
         </vxe-tip>
