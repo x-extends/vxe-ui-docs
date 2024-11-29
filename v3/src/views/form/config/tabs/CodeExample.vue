844ace5a5f8f949b817c11d4e2d253ff05e596f8
xuliangzhan <xlz26296x@163.com>
2024-11-29 17:50:12 +0800

    update docs

--- v3/src/views/form/config/tabs/CodeExample.vue
+++ v3/src/views/form/config/tabs/CodeExample.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <CodeLight path="form/config/tabs/Demo1">
+    <CodeLight path="form/config/tabs/Demo1" previewPath="/resource/docsImg/form_basics_tabs.gif">
       <template #tip>
         <vxe-tip status="primary" title="在页签中使用">
           当表单项非常多时，可以将表单拆分到多个页签中
