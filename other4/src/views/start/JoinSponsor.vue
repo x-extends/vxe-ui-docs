614f18118bc63610571454e916d175e4cc82fcf0
xuliangzhan <xlz26296x@163.com>
2024-12-10 14:50:19 +0800

    update docs

--- other4/src/views/start/JoinSponsor.vue
+++ other4/src/views/start/JoinSponsor.vue
@@ -1,20 +1,39 @@
 <template>
   <div>
-    <p class="tip">{{ appStore.pageTitle }} 是 MIT 开源的，使用完全免费。为了使项目能更好的持续维护，您可以通过成为赞助商方式来支持作者。</p>
-    <div class="content" style="padding-top: 50px">
-      <div class="sponsor-title">* 赞助（1000元/30天，1800元/60天）</div>
-      <div class="sponsor-desc">所有页面的右侧展示位（图片尺寸200*100）</div>
-      <div class="sponsor-img">
-        <img src="/static/sponsor/right.png" style="max-width: 800px;">
+    <p class="tip">{{ pageTitle }} 是 MIT 开源的，使用完全免费。为了使项目能更好的持续维护，您可以通过成为赞助商方式来支持作者。</p>
+    <div v-if="supportOptions.length" class="content" style="padding-top: 50px">
+      <div class="sponsor-title">
+        <span>* 赞助</span>
+        <span>（</span>
+        <span v-for="(item, index) in supportOptions" :key="index">
+          <span>{{ item.price }}元/{{ item.days }}天</span>
+          <span>，</span>
+        </span>
+        <span>）</span>
       </div>
+      <div class="sponsor-desc">当前站点所有页面的右侧展示位（图片尺寸200*100）</div>
+      <div class="sponsor-desc">联系邮箱：x_extends@163.com</div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { ref, computed } from 'vue'
 import { useAppStore } from '@/store/app'
+import axios from 'axios'
 
 const appStore = useAppStore()
+const siteBaseUrl = computed(() => appStore.siteBaseUrl)
+const pageTitle = computed(() => appStore.pageTitle)
+
+const supportOptions = ref<{
+  days: number
+  price: number
+}[]>([])
+
+axios.get(`${siteBaseUrl.value}/component-api/${process.env.VUE_APP_PACKAGE_NAME}-sponsor-config.json?v=${process.env.VUE_APP_DATE_NOW}`).then(res => {
+  supportOptions.value = res.data ? res.data.sponsors : []
+})
 </script>
 
 <style lang="scss" scoped>
@@ -25,9 +44,5 @@ const appStore = useAppStore()
 .sponsor-desc {
   font-size: 15px;
   margin-top: 5px;
-}
-.sponsor-img {
-  text-align: center;
-  margin-top: 40px;
 }
 </style>
