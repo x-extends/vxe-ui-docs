09953eb09545ee22035e48a016ecc86e584bf48b
xuliangzhan <xlz26296x@163.com>
2024-12-27 13:37:02 +0800

    update docs

--- v4/src/views/number-input/float/autoFill/Demo1.vue
+++ v4/src/views/number-input/float/autoFill/Demo1.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <vxe-number-input v-model="val1" placeholder="默认自动补0" type="float"></vxe-number-input>
-    <vxe-number-input v-model="val2" placeholder="关闭自动补0" type="float" :auto-fill=false></vxe-number-input>
+    <vxe-number-input v-model="val2" placeholder="关闭自动补0" type="float" :auto-fill="false"></vxe-number-input>
   </div>
 </template>
 
