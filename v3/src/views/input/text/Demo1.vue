5de4db6ed71d7bc9fa30ea0994ba0bd44fdad91c
xuliangzhan <xu_liangzhan@163.com>
2025-01-08 16:40:59 +0800

    update docs

--- v3/src/views/input/text/Demo1.vue
+++ v3/src/views/input/text/Demo1.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
     <vxe-input v-model="val1"></vxe-input>
-    <vxe-input v-model="val2" placeholder="请输入"></vxe-input>
-    <vxe-input v-model="val3" placeholder="禁用" disabled></vxe-input>
-    <vxe-input v-model="val4" disabled></vxe-input>
+    <vxe-input v-model="val2" placeholder="自定义提示语"></vxe-input>
+    <vxe-input v-model="val3" placeholder="去掉左右空格" trim></vxe-input>
+    <vxe-input v-model="val4" placeholder="禁用" disabled></vxe-input>
+    <vxe-input v-model="val5" disabled></vxe-input>
   </div>
 </template>
 
@@ -16,7 +17,8 @@ export default Vue.extend({
       val1: '',
       val2: '',
       val3: '',
-      val4: 'xxx'
+      val4: '',
+      val5: 'xxx'
     }
   }
 })
