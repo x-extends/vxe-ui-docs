70fc4d6104adbf640ecb360610fe68670e02e2ea
xuliangzhan <xu_liangzhan@163.com>
2025-01-08 12:41:03 +0800

    update docs

--- v3/src/views/form/basics/validate/manual/CodeExample.vue
+++ v3/src/views/form/basics/validate/manual/CodeExample.vue
@@ -3,6 +3,7 @@
     <CodeLight path="form/basics/validate/manual/Demo1">
       <template #tip>
         <vxe-tip status="primary" title="手动调用校验">
+          通过调用 <ApiLink name="form" prop="validate"/> 方法手动校验，校验失败会返回错误字段信息，校验成功则不返回值
         </vxe-tip>
       </template>
     </CodeLight>
