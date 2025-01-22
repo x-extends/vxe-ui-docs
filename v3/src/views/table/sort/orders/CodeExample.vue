2f71b78605c1cb1162497ec146b758eccb77b33e
xuliangzhan <xu_liangzhan@163.com>
2025-01-22 12:15:35 +0800

    update docs

--- v3/src/views/table/sort/orders/CodeExample.vue
+++ v3/src/views/table/sort/orders/CodeExample.vue
@@ -5,5 +5,11 @@
         <vxe-tip status="primary" title="自定义触发排序的轮询顺序">通过配置 <ApiLink name="table" prop="sort-config"/>.<ApiLink name="table" prop="orders"/> 自定义轮询顺序</vxe-tip>
       </template>
     </CodeLight>
+
+    <CodeLight path="table/sort/orders/Demo2">
+      <template #tip>
+        <vxe-tip status="primary">设置只能升序和倒序</vxe-tip>
+      </template>
+    </CodeLight>
   </div>
 </template>
