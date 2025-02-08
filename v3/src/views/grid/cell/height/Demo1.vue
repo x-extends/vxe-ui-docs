c87cda8fd5866490ce8cd2e6c858376fa1f523aa
xuliangzhan <xu_liangzhan@163.com>
2025-02-08 15:19:30 +0800

    update docs

--- v3/src/views/grid/cell/height/Demo1.vue
+++ v3/src/views/grid/cell/height/Demo1.vue
@@ -29,12 +29,13 @@ export default Vue.extend({
         { field: 'seq', type: 'seq', width: 70 },
         { field: 'name', title: 'Name' },
         { field: 'sex', title: 'Sex' },
-        { field: 'age', title: 'Age' }
+        { field: 'age', title: 'Age' },
+        { field: 'address', title: 'Address', width: 200 }
       ],
       data: [
         { id: 10001, name: 'Test1', role: 'Develop', sex: 'Man', age: 28, address: 'test abc' },
-        { id: 10002, name: 'Test2', role: 'Test', sex: 'Women', age: 22, address: 'Guangzhou' },
-        { id: 10003, name: 'Test3', role: 'PM', sex: 'Man', age: 32, address: 'Shanghai' },
+        { id: 10002, name: 'Test2', role: 'Test', sex: 'Women', age: 22, address: 'Guangzhou Guangzhou Guangzhou Guangzhou Guangzhou' },
+        { id: 10003, name: 'Test3', role: 'PM', sex: 'Man', age: 32, address: 'Shanghai Shanghai Shanghai Shanghai' },
         { id: 10004, name: 'Test4', role: 'Designer', sex: 'Women', age: 24, address: 'Shanghai' }
       ],
       footerData: [
