795a747bb0b8bbb8d48b776fe17df3c1bcefaff7
xuliangzhan <xu_liangzhan@163.com>
2025-02-11 17:02:10 +0800

    update docs

--- v3/src/views/grid/filter/tree/CodeExample.vue
+++ v3/src/views/grid/filter/tree/CodeExample.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <CodeLight path="grid/filter/tree/Demo1">
+    <CodeLight path="grid/filter/tree/Demo1" previewPath="/resource/docsImg/table_filter_tree.gif">
       <template #tip>
         <vxe-tip status="primary" title="树结构筛选">
           树结构会同时对每个层级的子节点进行筛选
--- v4/src/views/grid/sort/tree/CodeExample.vue
+++ v4/src/views/grid/sort/tree/CodeExample.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <CodeLight path="grid/sort/tree/Demo1">
+    <CodeLight path="grid/sort/tree/Demo1" previewPath="/resource/docsImg/table_sort_tree.gif">
       <template #tip>
         <vxe-tip status="primary" title="树结构排序">
           树结构会同时对每个层级的子节点进行排序
