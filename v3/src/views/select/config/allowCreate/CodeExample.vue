efc038dd0c6665eec9974c5ec7aef86100702278
xuliangzhan <xu_liangzhan@163.com>
2025-03-06 12:08:03 +0800

    update docs

--- v3/src/views/select/config/allowCreate/CodeExample.vue
+++ v3/src/views/select/config/allowCreate/CodeExample.vue
@@ -5,6 +5,9 @@
         <vxe-tip status="primary" title="可新增">
           通过 <ApiLink name="select" prop="allow-create"/> 启用可新增功能，当搜索后选项不存在时，可以手动新增选项
         </vxe-tip>
+        <vxe-tip status="error">
+          由于新增的数据是以输入值作为 value 的，应该避免输入值与选项列表中的值冲突，否则将会影响部分功能出问题
+        </vxe-tip>
       </template>
     </CodeLight>
   </div>
