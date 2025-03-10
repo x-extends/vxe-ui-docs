ddcdb1a4d7de33298c052687a155717be368be13
xuliangzhan <xu_liangzhan@163.com>
2025-03-10 19:10:07 +0800

    update docs

--- v4/src/views/modal/modal/fullscreen/CodeExample.vue
+++ v4/src/views/modal/modal/fullscreen/CodeExample.vue
@@ -3,7 +3,10 @@
     <CodeLight path="modal/modal/fullscreen/Demo1">
       <template #tip>
         <vxe-tip status="primary" title="默认全屏">
-          通过 fullscreen 开启窗口打开时默认全屏展示
+          通过 <ApiLink name="modal" prop="fullscreen"/> 开启窗口打开时默认全屏展示
+        </vxe-tip>
+        <vxe-tip status="error">
+          需要注意默认行为只会触发一次，如果需要记住上一次位置，可以配合 <ApiLink name="modal" prop="remember"/> 使用
         </vxe-tip>
       </template>
     </CodeLight>
