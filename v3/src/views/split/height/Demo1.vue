741bc06b2ac354aa8ff7e1a2f926bc61cee6184c
xuliangzhan <xu_liangzhan@163.com>
2025-03-27 13:37:35 +0800

    update docs

--- v3/src/views/split/height/Demo1.vue
+++ v3/src/views/split/height/Demo1.vue
@@ -2,10 +2,10 @@
   <div>
     <vxe-split height="300">
       <vxe-split-item>
-        <div style="height: 100%;background-color: #f3e1e1;">左侧</div>
+        <div style="height: 600px;background-color: #f3e1e1;">左侧</div>
       </vxe-split-item>
       <vxe-split-item>
-        <div style="height: 100%;background-color: #d8d8f9;">右侧</div>
+        <div style="height: 400px;background-color: #d8d8f9;">右侧</div>
       </vxe-split-item>
     </vxe-split>
   </div>
--- v3/src/views/split/vertical/Demo1.vue
+++ v3/src/views/split/vertical/Demo1.vue
@@ -2,10 +2,10 @@
   <div>
     <vxe-split height="300" border vertical>
       <vxe-split-item>
-        <div style="height: 100%;background-color: #f3e1e1;">顶部</div>
+        <div style="height: 200px;background-color: #f3e1e1;">顶部</div>
       </vxe-split-item>
       <vxe-split-item>
-        <div style="height: 100%;background-color: #d8d8f9;">底部</div>
+        <div style="height: 400px;background-color: #d8d8f9;">底部</div>
       </vxe-split-item>
     </vxe-split>
   </div>
