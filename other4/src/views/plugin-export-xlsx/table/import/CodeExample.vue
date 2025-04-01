d65c768c8da583fcb2cebf27ac169874e1be8db3
xuliangzhan <xu_liangzhan@163.com>
2025-04-01 12:48:32 +0800

    update docs

--- other4/src/views/plugin-export-xlsx/table/import/CodeExample.vue
+++ other4/src/views/plugin-export-xlsx/table/import/CodeExample.vue
@@ -3,7 +3,7 @@
     <CodeLight path="plugin-export-xlsx/table/import/Demo1">
       <template #tip>
         <vxe-tip status="primary" title="导入 XLSX 文件">
-          导入数据时需要列字段匹配上，可以在导出时将参数设置为 original 源数据导出
+          导入数据时需要列字段匹配上，可以在导出时将参数设置为 original 源数据导出，支持自动识别表头 field 和 title 导入
         </vxe-tip>
       </template>
     </CodeLight>
