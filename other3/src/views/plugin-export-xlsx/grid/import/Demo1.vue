a9c87972ff99811991d052f783806bcb9d2e3a64
xuliangzhan <xu_liangzhan@163.com>
2025-04-03 17:26:24 +0800

    update docs

--- other3/src/views/plugin-export-xlsx/grid/import/Demo1.vue
+++ other3/src/views/plugin-export-xlsx/grid/import/Demo1.vue
@@ -6,8 +6,8 @@
   </div>
 </template>
 
-<script lang="ts" setup>
-import { ref, reactive } from 'vue'
+<script lang="ts">
+import Vue from 'vue'
 import type { VxeGridInstance, VxeGridProps } from 'vxe-table'
 
 interface RowVO {
@@ -19,40 +19,46 @@ interface RowVO {
   address: string
 }
 
-const gridRef = ref<VxeGridInstance>()
+export default Vue.extend({
+  data () {
+    const gridOptions: VxeGridProps<RowVO> = {
+      border: true,
+      columns: [
+        { type: 'seq', width: 70 },
+        { field: 'name', title: 'Name' },
+        { field: 'sex', title: 'Sex' },
+        { field: 'age', title: 'Age' }
+      ],
+      data: [
+        { id: 10001, name: 'Test1', role: 'Develop', sex: 'Man', age: 28, address: 'test abc' },
+        { id: 10002, name: 'Test2', role: 'Test', sex: 'Women', age: 22, address: 'Guangzhou' },
+        { id: 10003, name: 'Test3', role: 'PM', sex: 'Man', age: 32, address: 'Shanghai' },
+        { id: 10004, name: 'Test4', role: 'Designer', sex: 'Women', age: 24, address: 'Shanghai' }
+      ]
+    }
 
-const gridOptions = reactive<VxeGridProps<RowVO>>({
-  border: true,
-  columns: [
-    { type: 'seq', width: 70 },
-    { field: 'name', title: 'Name' },
-    { field: 'sex', title: 'Sex' },
-    { field: 'age', title: 'Age' }
-  ],
-  data: [
-    { id: 10001, name: 'Test1', role: 'Develop', sex: 'Man', age: 28, address: 'test abc' },
-    { id: 10002, name: 'Test2', role: 'Test', sex: 'Women', age: 22, address: 'Guangzhou' },
-    { id: 10003, name: 'Test3', role: 'PM', sex: 'Man', age: 32, address: 'Shanghai' },
-    { id: 10004, name: 'Test4', role: 'Designer', sex: 'Women', age: 24, address: 'Shanghai' }
-  ]
+    return {
+      gridOptions
+    }
+  },
+  methods: {
+    exportEvent () {
+      const $grid = this.$refs.gridRef as VxeGridInstance<RowVO>
+      if ($grid) {
+        $grid.exportData({
+          type: 'xlsx',
+          original: true
+        })
+      }
+    },
+    importEvent () {
+      const $grid = this.$refs.gridRef as VxeGridInstance<RowVO>
+      if ($grid) {
+        $grid.importData({
+          types: ['xlsx']
+        })
+      }
+    }
+  }
 })
-
-const exportEvent = () => {
-  const $grid = gridRef.value
-  if ($grid) {
-    $grid.exportData({
-      type: 'xlsx',
-      original: true
-    })
-  }
-}
-
-const importEvent = () => {
-  const $grid = gridRef.value
-  if ($grid) {
-    $grid.importData({
-      types: ['xlsx']
-    })
-  }
-}
 </script>
