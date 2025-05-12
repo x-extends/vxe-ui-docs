3dcf59c2c0ad92768aacc909d748b0acd861f777
xuliangzhan <xu_liangzhan@163.com>
2025-05-12 13:35:15 +0800

    update docs

--- v3/src/views/print/page/margin/CodeExample.vue
+++ v3/src/views/print/page/margin/CodeExample.vue
@@ -10,7 +10,7 @@
 
     <CodeLight path="print/page/margin/Demo2">
       <template #tip>
-        <vxe-tip status="primary" title="设置头部和维护高度">
+        <vxe-tip status="primary" title="设置头部和页尾高度">
           通过设置 <ApiLink name="print" prop="page-style"/>.<ApiLink name="print" prop="header"/>.<ApiLink name="print" prop="height"/> 设置打印的页面头部高度、<ApiLink name="print" prop="page-style"/>.<ApiLink name="print" prop="footer"/>.<ApiLink name="print" prop="height"/> 设置打印的页面尾部高度
         </vxe-tip>
       </template>
