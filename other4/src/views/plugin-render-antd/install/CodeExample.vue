47d927222cdc84fa41f40260aa59d7ae071f6428
xuliangzhan <xu_liangzhan@163.com>
2025-06-29 00:03:07 +0800

    update docs

--- other4/src/views/plugin-render-antd/install/CodeExample.vue
+++ other4/src/views/plugin-render-antd/install/CodeExample.vue
@@ -59,8 +59,8 @@
             import 'vxe-pc-ui/lib/style.css'
             import VxeUITable from 'vxe-table'
             import 'vxe-table/lib/style.css'
-            import VxeUIPluginRenderAntd from '@vxe-ui/plugin-render-element'
-            import '@vxe-ui/plugin-render-element/dist/style.css'
+            import VxeUIPluginRenderAntd from '@vxe-ui/plugin-render-antd'
+            import '@vxe-ui/plugin-render-antd/dist/style.css'
             // ...
 
             import { AInput, AInputNumber, ASelect, ACascader, ADatePicker, ATimePicker } from 'ant-design-vue'
