37d74f96e7e5d1b3c8f407ecb78621e68b297a31
xuliangzhan <xu_liangzhan@163.com>
2025-07-11 11:55:21 +0800

    update docs

--- v3/src/views/tree/search/light/CodeExample.vue
+++ v3/src/views/tree/search/light/CodeExample.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <CodeLight path="tree/search/light/Demo1">
+    <CodeLight path="tree/search/light/Demo1" previewPath="/resource/docsImg/tree_search_light.gif">
       <template #tip>
         <vxe-tip status="primary" title="搜索高亮关键字">
         </vxe-tip>
