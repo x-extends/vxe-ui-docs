b9d2e1ef96f1dc85591c24d87e0dfe6e6d0d190a
xuliangzhan <xu_liangzhan@163.com>
2025-07-21 13:39:42 +0800

    update docs

--- v3/src/views/date-range-picker/date/disabledMethod/Demo2.vue
+++ v3/src/views/date-range-picker/date/disabledMethod/Demo2.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <vxe-date-range-picker v-model="val1" placeholder="限制只能选择周五" :disabled-method="disabledDateMethod"></vxe-date-range-picker>
+    <vxe-date-range-picker v-model="val1" placeholder="限制只能选择周五" :disabled-method="disabledDateMethod" clearable></vxe-date-range-picker>
   </div>
 </template>
 
