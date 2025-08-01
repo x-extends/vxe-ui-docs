0897ab6a1ae2cca35a6833e1c8558964c499ab3e
xuliangzhan <xu_liangzhan@163.com>
2025-08-01 14:57:46 +0800

    update docs

--- other4/vite.config.ts
+++ other4/vite.config.ts
@@ -35,7 +35,19 @@ export default defineConfig(({ mode, command }) => {
     },
     server: {
       host: '0.0.0.0',
-      port: Number(env.VITE_APP_SERVER_PORT)
+      port: Number(env.VITE_APP_SERVER_PORT),
+      proxy: {
+        '/example/': {
+          target: 'https://vxeui.com',
+          changeOrigin: true,
+          secure: false
+        },
+        '/resource/': {
+          target: 'https://vxeui.com',
+          changeOrigin: true,
+          secure: false
+        }
+      }
     },
     build: {
       sourcemap: command === 'build' ? false : 'inline',
