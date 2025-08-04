8ed0c0e7e386ec2c8e79873458c3841ad8f9124c
xuliangzhan <xu_liangzhan@163.com>
2025-08-04 18:13:49 +0800

    update docs

--- other3/src/views/plugin-export-xlsx/grid/advanced/CodeExample.vue
+++ other3/src/views/plugin-export-xlsx/grid/advanced/CodeExample.vue
@@ -5,6 +5,9 @@
         <vxe-tip status="primary" title="高级导出">
           通过调用 openExport 方法打开导出面板，可以自定义更多的设置
         </vxe-tip>
+        <vxe-tip status="error">
+          如果想导出图片，需确保图片是有效链接，且允许跨域获取，否则不支持导出图片
+        </vxe-tip>
       </template>
     </CodeLight>
   </div>
