6a050c26b282157552ce68156a9311eb6b88a5eb
xuliangzhan <xu_liangzhan@163.com>
2025-08-15 12:43:54 +0800

    update docs

--- v3/src/router/gantt.ts
+++ v3/src/router/gantt.ts
@@ -2,7 +2,7 @@ import { RouteConfig } from 'vue-router'
 
 import RouteLayout from '@/components/RouteLayout.vue'
 
-export const ganttRouteConfig: RouteRecordRaw = {
+export const ganttRouteConfig: RouteConfig = {
   path: 'gantt',
   component: RouteLayout,
   children: [
