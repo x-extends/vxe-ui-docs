4bd72b87a861c7dca8a49289aec0c81ff7aa380a
xuliangzhan <xu_liangzhan@163.com>
2025-08-20 11:03:50 +0800

    update docs

--- v3/src/views/select/base/defaultValue/CodeExample.vue
+++ v3/src/views/select/base/defaultValue/CodeExample.vue
@@ -5,6 +5,9 @@
         <vxe-tip status="primary" title="默认值">
           通过 <ApiLink name="select" prop="default-config"/>.<ApiLink name="select" prop="selectMode"/> 可以提前设置默认值，会在初始化的时候触发一次默认值行为
         </vxe-tip>
+        <vxe-tip status="error">
+          当绑定值为 null 或 undefined 时且首次初始化时自动触发默认值行为
+        </vxe-tip>
       </template>
     </CodeLight>
 
