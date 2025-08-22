b67dd429fb4bc2f45ba4ca4f94c8e6db34e59e9d
xuliangzhan <xu_liangzhan@163.com>
2025-08-22 18:07:02 +0800

    update docs

--- v3/src/router/gantt.ts
+++ v3/src/router/gantt.ts
@@ -60,7 +60,9 @@ export const ganttRouteConfig: RouteConfig = {
       component: RouteLayout,
       children: [
         { path: 'highlight', name: 'ComponentGanttRadioHighlight', component: () => import('@/views/gantt/radio/highlight/CodeExample.vue') },
-        { path: 'checkMethod', name: 'ComponentGanttRadioCheckMethod', component: () => import('@/views/gantt/radio/checkMethod/CodeExample.vue') }
+        { path: 'checkMethod', name: 'ComponentGanttRadioCheckMethod', component: () => import('@/views/gantt/radio/checkMethod/CodeExample.vue') },
+        { path: 'trigger', name: 'ComponentGanttRadioTrigger', component: () => import('@/views/gantt/radio/trigger/CodeExample.vue') },
+        { path: 'visibleMethod', name: 'ComponentGanttRadioVisibleMethod', component: () => import('@/views/gantt/radio/visibleMethod/CodeExample.vue') }
       ]
     },
     {
@@ -69,7 +71,13 @@ export const ganttRouteConfig: RouteConfig = {
       children: [
         { path: 'highlight', name: 'ComponentGanttCheckboxHighlight', component: () => import('@/views/gantt/checkbox/highlight/CodeExample.vue') },
         { path: 'field', name: 'ComponentGanttCheckboxField', component: () => import('@/views/gantt/checkbox/field/CodeExample.vue') },
-        { path: 'checkMethod', name: 'ComponentGanttCheckboxCheckMethod', component: () => import('@/views/gantt/checkbox/checkMethod/CodeExample.vue') }
+        { path: 'checkMethod', name: 'ComponentGanttCheckboxCheckMethod', component: () => import('@/views/gantt/checkbox/checkMethod/CodeExample.vue') },
+        { path: 'batch', name: 'ComponentGanttCheckboxBatch', component: () => import('@/views/gantt/checkbox/batch/CodeExample.vue') },
+        { path: 'range', name: 'ComponentGanttCheckboxRange', component: () => import('@/views/gantt/checkbox/range/CodeExample.vue') },
+        { path: 'reserve', name: 'ComponentGanttCheckboxReserve', component: () => import('@/views/gantt/checkbox/reserve/CodeExample.vue') },
+        { path: 'showReserveStatus', name: 'ComponentGanttCheckboxShowReserveStatus', component: () => import('@/views/gantt/checkbox/showReserveStatus/CodeExample.vue') },
+        { path: 'trigger', name: 'ComponentGanttCheckboxTrigger', component: () => import('@/views/gantt/checkbox/trigger/CodeExample.vue') },
+        { path: 'visibleMethod', name: 'ComponentGanttCheckboxVisibleMethod', component: () => import('@/views/gantt/checkbox/visibleMethod/CodeExample.vue') }
       ]
     },
     {
