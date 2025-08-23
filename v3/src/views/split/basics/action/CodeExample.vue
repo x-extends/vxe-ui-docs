4944787f0c72f2a43c6414803994240221fd575a
xuliangzhan <xu_liangzhan@163.com>
2025-08-23 20:20:06 +0800

    update docs

--- v3/src/views/split/basics/action/CodeExample.vue
+++ v3/src/views/split/basics/action/CodeExample.vue
@@ -2,7 +2,9 @@
   <div>
     <CodeLight path="split/basics/action/Demo1">
       <template #tip>
-        <vxe-tip status="primary" title="折叠按钮"></vxe-tip>
+        <vxe-tip status="primary" title="可折叠">
+          通过 <ApiLink name="split" prop="show-action"/> 设置指定面板是否显示折叠按钮
+        </vxe-tip>
       </template>
     </CodeLight>
 
--- v3/src/views/split/basics/actionConfig/CodeExample.vue
+++ v3/src/views/split/basics/actionConfig/CodeExample.vue
@@ -3,7 +3,7 @@
     <CodeLight path="split/basics/actionConfig/Demo1">
       <template #tip>
         <vxe-tip status="primary" title="折叠按钮方向">
-          通过 <ApiLink name="split" prop="action-config"/>。<ApiLink name="split" prop="direction"/> 设置折叠按钮方向
+          通过 <ApiLink name="split" prop="action-config"/>.<ApiLink name="split" prop="direction"/> 设置折叠按钮方向
         </vxe-tip>
       </template>
     </CodeLight>
