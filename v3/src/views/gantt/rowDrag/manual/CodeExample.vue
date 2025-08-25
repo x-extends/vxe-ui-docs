546ef015270337295c3c9b260a4ffd05548415c7
xuliangzhan <xu_liangzhan@163.com>
2025-08-25 10:53:07 +0800

    update docs

--- v3/src/views/gantt/rowDrag/manual/CodeExample.vue
+++ v3/src/views/gantt/rowDrag/manual/CodeExample.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <CodeLight path="gantt/rowDrag/manual/Demo1">
+    <CodeLight path="gantt/rowDrag/manual/Demo1" previewPath="/resource/docsImg/gantt_row_drag_manual.gif">
       <template #tip>
         <vxe-tip status="primary" title="手动移动行">
           通过调用 <ApiLink name="gantt" prop="moveRowTo"/> 方法来手动移动行的顺序
--- v4/src/views/gantt/scales/quarterMonthDate/CodeExample.vue
+++ v4/src/views/gantt/scales/quarterMonthDate/CodeExample.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <CodeLight path="gantt/scales/quarterMonthDate/Demo1">
+    <CodeLight path="gantt/scales/quarterMonthDate/Demo1" previewPath="/resource/docsImg/gantt_scales_quarter_month_date.gif">
       <template #tip>
         <vxe-tip status="primary" title="季-月-日"></vxe-tip>
       </template>
