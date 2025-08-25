cc897891dcb4c107c5950ed2b440badefd8a495e
xuliangzhan <xu_liangzhan@163.com>
2025-08-25 10:17:24 +0800

    update docs

--- v3/src/views/gantt/rowDrag/manual/CodeExample.vue
+++ v3/src/views/gantt/rowDrag/manual/CodeExample.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <CodeLight path="gantt/rowDrag/manual/Demo1" previewPath="/resource/docsImg/table_row_drag_manual.gif">
+    <CodeLight path="gantt/rowDrag/manual/Demo1">
       <template #tip>
         <vxe-tip status="primary" title="手动移动行">
           通过调用 <ApiLink name="gantt" prop="moveRowTo"/> 方法来手动移动行的顺序
