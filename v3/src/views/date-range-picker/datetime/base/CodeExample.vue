c2cd0d84e2ecf2176e99802543bb6e709e9860ba
xuliangzhan <xu_liangzhan@163.com>
2025-09-12 11:39:55 +0800

    update docs

--- v3/src/views/date-range-picker/datetime/base/CodeExample.vue
+++ v3/src/views/date-range-picker/datetime/base/CodeExample.vue
@@ -3,12 +3,18 @@
     <CodeLight path="date-range-picker/datetime/base/Demo1">
       <template #tip>
         <vxe-tip status="primary" title="绑定值">绑定值，绑定的2个值用逗号分隔</vxe-tip>
+        <vxe-tip status="error">
+          默认是根据绑定值自动识别，如果需要固定类型，☝️通过 <ApiLink name="date-range-picker" prop="value-type"/>='string' 设置
+        </vxe-tip>
       </template>
     </CodeLight>
 
     <CodeLight path="date-range-picker/datetime/base/Demo2">
       <template #tip>
         <vxe-tip status="primary">数组类型，绑定的2个值放数组中</vxe-tip>
+        <vxe-tip status="error">
+          默认是根据绑定值自动识别，如果需要固定类型，☝️通过 <ApiLink name="date-range-picker" prop="value-type"/>='array' 设置
+        </vxe-tip>
       </template>
     </CodeLight>
 
