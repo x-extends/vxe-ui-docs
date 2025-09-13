00efe2e30276511c002a4770fdb130b450c3beac
xuliangzhan <xu_liangzhan@163.com>
2025-09-13 21:50:52 +0800

    update docs

--- v4/src/views/form/basics/validate/showMessage/CodeExample.vue
+++ v4/src/views/form/basics/validate/showMessage/CodeExample.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <CodeLight path="basics/config/validate/showMessage/Demo1">
+    <CodeLight path="form/basics/validate/showMessage/Demo1">
       <template #tip>
         <vxe-tip status="primary" title="显示提示消息">
           通过设置 <ApiLink name="grid" prop="valid-config"/>.<ApiLink name="grid" prop="showErrorMessage"/> 是否显示校验提示
