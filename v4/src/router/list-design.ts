351892265eda03a25984fa8c7a9a1b0c620c418f
xuliangzhan <xu_liangzhan@163.com>
2025-09-22 18:05:00 +0800

    update docs

--- v4/src/router/list-design.ts
+++ v4/src/router/list-design.ts
@@ -6,25 +6,9 @@ export const listDesignRouteConfig: RouteRecordRaw = {
   path: 'list-design',
   component: RouteLayout,
   children: [
-    {
-      path: 'base',
-      name: 'ComponentListDesignBase',
-      component: () => import('@/views/list-design/base/CodeExample.vue')
-    },
-    {
-      path: 'mobile',
-      name: 'ComponentListDesignMobile',
-      component: () => import('@/views/list-design/mobile/CodeExample.vue')
-    },
-    {
-      path: 'pcAndMobile',
-      name: 'ComponentListDesignPcAndMobile',
-      component: () => import('@/views/list-design/pcAndMobile/CodeExample.vue')
-    },
-    {
-      path: 'formAndList',
-      name: 'ComponentListDesignFormAndList',
-      component: () => import('@/views/list-design/formAndList/CodeExample.vue')
-    }
+    { path: 'base', name: 'ComponentListDesignBase', component: () => import('@/views/list-design/base/CodeExample.vue') },
+    { path: 'mobile', name: 'ComponentListDesignMobile', component: () => import('@/views/list-design/mobile/CodeExample.vue') },
+    { path: 'pcAndMobile', name: 'ComponentListDesignPcAndMobile', component: () => import('@/views/list-design/pcAndMobile/CodeExample.vue') },
+    { path: 'formAndList', name: 'ComponentListDesignFormAndList', component: () => import('@/views/list-design/formAndList/CodeExample.vue') }
   ]
 }
