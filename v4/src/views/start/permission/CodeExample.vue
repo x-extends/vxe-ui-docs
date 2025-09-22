dffbad1a46277a4c87bc204bfa2195fcf4da037d
xuliangzhan <xu_liangzhan@163.com>
2025-09-22 14:40:18 +0800

    update docs

--- v4/src/views/start/permission/CodeExample.vue
+++ v4/src/views/start/permission/CodeExample.vue
@@ -33,20 +33,34 @@
                 }
               }
             })
-
-            if (VxeUI.permission.checkVisible('xxx')) {
-              // 判断是否有权限
-            }
           </pre-code>
-          <div>在组件中指定 permissionCode 权限码，当无权限时会被自动隐藏</div>
+          <div>在组件中使用，指定 permissionCode 权限码，当无权限时会被自动隐藏</div>
           <pre-code class="html">
             &lt;vxe-button permission-code="xxx"&gt;按钮&lt;/vxe-button&gt;
             &lt;vxe-link permission-code="xxx"&gt;链接&lt;/vxe-link&gt;
           </pre-code>
-          <div>如果多个权限码，支持分隔符：|（或）</div>
+          <div>在组件中使用，如果多个权限码，支持分隔符：|（或）</div>
           <pre-code class="html">
             &lt;vxe-button permission-code="aaa|bbb"&gt;按钮&lt;/vxe-button&gt;
             &lt;vxe-link permission-code="aaa|bbb"&gt;链接&lt;/vxe-link&gt;
+          </pre-code>
+          <div>如果在 js 中使用</div>
+          <pre-code class="javascript">
+            if (VxeUI.permission.checkVisible('xxx')) {
+              // 判断是否有可视权限
+            }
+
+            if (VxeUI.permission.checkDisable('xxx')) {
+              // 判断是否被禁用
+            }
+
+            const permissionInfo = VxeUI.permission.getCheckInfo('xxx')
+            if (permissionInfo.visible) {
+              // 判断是否有可视权限
+            }
+            if (permissionInfo.disabled) {
+              // 判断是否被禁用
+            }
           </pre-code>
         </pre>
       </template>
