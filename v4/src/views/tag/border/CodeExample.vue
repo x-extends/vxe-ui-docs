8eacfa0b071ac45d3744caa70ee8f72fb82a0b2c
xuliangzhan <xu_liangzhan@163.com>
2025-09-29 17:02:14 +0800

    update docs

--- v4/src/views/tag/border/CodeExample.vue
+++ v4/src/views/tag/border/CodeExample.vue
@@ -2,7 +2,9 @@
   <div>
     <CodeLight path="tag/border/Demo1">
       <template #tip>
-        <vxe-tip status="primary" title="边框"></vxe-tip>
+        <vxe-tip status="primary" title="边框">
+          参数 <ApiLink name="tag" prop="border"/> 设置边框
+        </vxe-tip>
       </template>
     </CodeLight>
   </div>
