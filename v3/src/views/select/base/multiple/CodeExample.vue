c9b79e1371fb12d2f2e6bceed6c2c7bc10520167
xuliangzhan <xu_liangzhan@163.com>
2025-10-14 16:24:45 +0800

    update docs

--- v3/src/views/select/base/multiple/CodeExample.vue
+++ v3/src/views/select/base/multiple/CodeExample.vue
@@ -2,9 +2,17 @@
   <div>
     <CodeLight path="select/base/multiple/Demo1">
       <template #tip>
-        <vxe-tip status="primary">多选</vxe-tip>
+        <vxe-tip status="primary" title="多选"></vxe-tip>
         <vxe-tip status="error">
           需要注意，value 值不能包含逗号，逗号属于分割值
+        </vxe-tip>
+      </template>
+    </CodeLight>
+
+    <CodeLight path="select/base/multiple/Demo2">
+      <template #tip>
+        <vxe-tip status="primary">
+          当已选很多时，可以设置最多显示多少个已选
         </vxe-tip>
       </template>
     </CodeLight>
---
+++ v4/src/views/select/config/multiple/Demo2.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <vxe-select v-model="val1" v-bind="selectOptions"></vxe-select>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { VxeSelectProps } from 'vxe-pc-ui'
+
+const val1 = ref([])
+const selectOptions: VxeSelectProps = {
+  multiple: true,
+  clearable: true,
+  placeholder: '多选',
+  multiCharOverflow: 2,
+  options: [
+    { value: 1001, label: 'table' },
+    { value: 1002, label: 'grid' },
+    { value: 1003, label: 'button' },
+    { value: 1004, label: 'toolbar' },
+    { value: 1005, label: 'tooltip' },
+    { value: 1006, label: 'pager' },
+    { value: 1007, label: 'print' },
+    { value: 1008, label: 'export' },
+    { value: 1009, label: 'import' },
+    { value: 1010, label: 'select' },
+    { value: 1012, label: 'checkbox' },
+    { value: 1013, label: 'group' }
+  ]
+}
+</script>
