0521faed91f2c1c87c1b760c560a6c8e017db7e2
xuliangzhan <xu_liangzhan@163.com>
2025-10-29 17:09:24 +0800

    update docs

--- v4/src/App.vue
+++ v4/src/App.vue
@@ -26,17 +26,22 @@ axios.get(`${siteBaseUrl.value}/component-api/vxe-version.json?v=${import.meta.e
 
 const voteKey = 'VXE_VOTE_2025_4'
 if (!localStorage.getItem(voteKey)) {
-  VxeUI.modal.confirm({
+  VxeUI.modal.alert({
     width: 500,
-    title: 'vxe-table 正在参加 Gitee 2025 最受欢迎的开源软件投票活动',
+    title: '🔥vxe-table 正在参加 Gitee 2025 最受欢迎的开源软件投票活动',
     confirmButtonText: '去投票',
     cancelButtonText: '忽略，不再弹出',
+    showCancelButton: true,
     slots: {
       default () {
         return <div>
-        <div>如果该项目对您有帮助，快来投票！</div>
-        <div><vxe-link status="primary" href="https://gitee.com/activity/2025opensource?ident=IX0B8Q" target="_blank">https://gitee.com/activity/2025opensource?ident=IX0B8Q</vxe-link></div>
-      </div>
+          <div>如果该项目 <vxe-link href="https://gitee.com/x-extends/vxe-table" target="_blank">vxe-table</vxe-link> 对您有帮助，快来给我投票吧！</div>
+          <div>
+            <vxe-text click-to-copy>
+              <vxe-link status="primary" href="https://gitee.com/activity/2025opensource?ident=IX0B8Q" target="_blank">https://gitee.com/activity/2025opensource?ident=IX0B8Q</vxe-link>
+            </vxe-text>
+          </div>
+        </div>
       }
     }
   }).then((type) => {
