65d7f426d93795eebb46543137e6eb225fe15ee3
xuliangzhan <xu_liangzhan@163.com>
2025-11-16 17:23:59 +0800

    update docs

--- v3/src/views/tree/base/icon/CodeExample.vue
+++ v3/src/views/tree/base/icon/CodeExample.vue
@@ -5,5 +5,13 @@
         <vxe-tip status="primary" title="自定义图标"></vxe-tip>
       </template>
     </CodeLight>
+
+    <CodeLight path="tree/base/icon/Demo2">
+      <template #tip>
+        <vxe-tip status="primary">
+          自定义显示标题图标
+        </vxe-tip>
+      </template>
+    </CodeLight>
   </div>
 </template>
