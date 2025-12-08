03f73c5b465fe7b2d123cccf2763d619a8c0a0a5
xuliangzhan <xu_liangzhan@163.com>
2025-12-08 14:26:58 +0800

    update docs

--- v4/src/views/tree-select/base/hierarchy/CodeExample.vue
+++ v4/src/views/tree-select/base/hierarchy/CodeExample.vue
@@ -5,6 +5,9 @@
         <vxe-tip status="primary" title="层级结构">
           支持渲染带有树层级结构的数据，例如 <ApiLink name="tree-select" prop="tree-config"/>.<ApiLink name="tree-select" prop="title-field"/>、<ApiLink name="tree-select" prop="tree-config"/>.<ApiLink name="tree-select" prop="children-field"/>
         </vxe-tip>
+        <vxe-tip status="error">
+          该模式下仅支持部分功能，不支持虚拟滚动，数据量大过时不建议使用
+        </vxe-tip>
       </template>
     </CodeLight>
   </div>
