a3c09ca7d4ae7498dcc957cc984e3e4d03ed35c6
xuliangzhan <xu_liangzhan@163.com>
2025-12-10 13:41:18 +0800

    update docs

--- v3/src/views/tree/drag/cossDrag/CodeExample.vue
+++ v3/src/views/tree/drag/cossDrag/CodeExample.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <CodeLight path="tree/drag/cossDrag/Demo1">
+    <CodeLight path="tree/drag/cossDrag/Demo1" previewPath="/resource/pluginImg/tree_drag_coss_drag.gif">
       <template #tip>
         <vxe-tip status="primary" title="跨层级拖拽">
           通过 <ApiLink name="tree" prop="drag-config"/>.<ApiLink name="tree" prop="isCrossDrag"/> 启用跨层级拖拽
