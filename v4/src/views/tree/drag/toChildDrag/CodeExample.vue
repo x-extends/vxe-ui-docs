5beb65c4a76d4583dc0d19a31f3d5e21ad420c34
xuliangzhan <xu_liangzhan@163.com>
2025-12-10 14:05:33 +0800

    update docs

--- v4/src/views/tree/drag/toChildDrag/CodeExample.vue
+++ v4/src/views/tree/drag/toChildDrag/CodeExample.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <CodeLight path="tree/drag/toChildDrag/Demo1">
+    <CodeLight path="tree/drag/toChildDrag/Demo1" previewPath="/resource/docsImg/tree_drag_to_child_drag.gif">
       <template #tip>
         <vxe-tip status="primary" title="拖拽转成子节点">
           通过 <ApiLink name="tree" prop="drag-config"/>.<ApiLink name="tree" prop="isToChildDrag"/> 启用便捷拖拽成子节点，拖拽的同时按住 Ctrl 键可以自动拖放到该节点的子级
