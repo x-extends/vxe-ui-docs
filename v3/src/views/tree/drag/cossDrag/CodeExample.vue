b5d63ba069fbeff4efa77e51fcc0cfca789f9cf4
xuliangzhan <xu_liangzhan@163.com>
2025-12-13 19:38:17 +0800

    update docs

--- v3/src/views/tree/drag/cossDrag/CodeExample.vue
+++ v3/src/views/tree/drag/cossDrag/CodeExample.vue
@@ -5,6 +5,9 @@
         <vxe-tip status="primary" title="跨层级拖拽">
           通过 <ApiLink name="tree" prop="drag-config"/>.<ApiLink name="tree" prop="isCrossDrag"/> 启用跨层级拖拽
         </vxe-tip>
+        <vxe-tip status="error">
+          拖拽功能仅支持 <ApiLink name="table" prop="transform"/> 模式
+        </vxe-tip>
       </template>
     </CodeLight>
   </div>
--- v4/src/views/tree/drag/sort/CodeExample.vue
+++ v4/src/views/tree/drag/sort/CodeExample.vue
@@ -6,7 +6,8 @@
           通过 <ApiLink name="tree" prop="drag"/> 启用行拖拽排序功能
         </vxe-tip>
         <vxe-tip status="error">
-          拖拽排序树的数据是不会改变数据源的，如果需要改变数据可以放弃内置拖拽功能自行去实现排序功能
+          <div>拖拽功能仅支持 <ApiLink name="table" prop="transform"/> 模式</div>
+          <div>拖拽排序树的数据是不会改变数据源的，如果需要改变数据可以放弃内置拖拽功能自行去实现排序功能</div>
         </vxe-tip>
       </template>
     </CodeLight>
