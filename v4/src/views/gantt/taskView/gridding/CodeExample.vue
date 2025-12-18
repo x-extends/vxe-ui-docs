deec8aba1f30f52690cbc11cb3b39ecf0d82c3a4
xuliangzhan <xu_liangzhan@163.com>
2025-12-18 20:50:53 +0800

    update docs

--- v4/src/views/gantt/taskView/gridding/CodeExample.vue
+++ v4/src/views/gantt/taskView/gridding/CodeExample.vue
@@ -3,7 +3,7 @@
     <CodeLight path="gantt/taskView/gridding/Demo1">
       <template #tip>
         <vxe-tip status="primary" title="网格样式">
-          通过 <ApiLink name="gantt" prop="task-view-config"/>.<ApiLink name="gantt" prop="viewStyle"/>.<ApiLink name="gantt" prop="cellStyle"/> 设置任务视图单元格样式
+          通过 <ApiLink name="gantt" prop="task-view-config"/>.<ApiLink name="gantt" prop="gridding"/>.<ApiLink name="gantt" prop="cellStyle"/> 设置任务视图单元格样式
         </vxe-tip>
       </template>
     </CodeLight>
