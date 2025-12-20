157cf87ecd8fbddaf914fcfb5871ff8c84fee1d8
xuliangzhan <xu_liangzhan@163.com>
2025-12-20 17:09:19 +0800

    update docs

--- v4/src/views/table/rowGroup/template/groupContent/CodeExample.vue
+++ v4/src/views/table/rowGroup/template/groupContent/CodeExample.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <CodeLight path="grid/rowGroup/template/default/Demo1">
+    <CodeLight path="table/rowGroup/template/groupContent/Demo1">
       <template #tip>
         <vxe-tip status="primary" title="自定义分组内容模版">
-          通过行分组的插槽模板 <ApiLink name="grid" prop="group-content"/> 来自定义
+          通过 <ApiLink name="column" prop="group-content"/> 来自定义分组内容模版
         </vxe-tip>
       </template>
     </CodeLight>
