0dba17d705951e44a52627818ae6c0d6d870afa0
xuliangzhan <xu_liangzhan@163.com>
2025-12-30 18:25:24 +0800

    update docs

--- v3/src/views/password-input/clearable/Demo1.vue
+++ v3/src/views/password-input/clearable/Demo1.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
-    <vxe-input v-model="userName" placeholder="用户名"></vxe-input>
-    <vxe-password-input v-model="val1" placeholder="可清除" clearable></vxe-password-input>
+    <vxe-form>
+      <vxe-form-item>
+        <vxe-input v-model="userName" placeholder="用户名"></vxe-input>
+      </vxe-form-item>
+      <vxe-form-item>
+        <vxe-password-input v-model="val1" placeholder="可清除" clearable></vxe-password-input>
+      </vxe-form-item>
+    </vxe-form>
   </div>
 </template>
 
--- v4/src/views/password-input/size/Demo1.vue
+++ v4/src/views/password-input/size/Demo1.vue
@@ -1,10 +1,40 @@
 <template>
   <div>
-    <vxe-input v-model="userName" placeholder="用户名"></vxe-input>
-    <vxe-password-input v-model="val1" placeholder="默认尺寸"></vxe-password-input>
-    <vxe-password-input v-model="val2" size="medium" placeholder="中等尺寸"></vxe-password-input>
-    <vxe-password-input v-model="val3" size="small" placeholder="小型尺寸"></vxe-password-input>
-    <vxe-password-input v-model="val4" size="mini" placeholder="超小尺寸"></vxe-password-input>
+    <vxe-form>
+      <vxe-form-item>
+        <vxe-input v-model="userName" placeholder="用户名"></vxe-input>
+      </vxe-form-item>
+      <vxe-form-item>
+        <vxe-password-input v-model="val1" placeholder="默认尺寸"></vxe-password-input>
+      </vxe-form-item>
+    </vxe-form>
+
+    <vxe-form>
+      <vxe-form-item>
+        <vxe-input v-model="userName" size="medium" placeholder="用户名"></vxe-input>
+      </vxe-form-item>
+      <vxe-form-item>
+        <vxe-password-input v-model="val2" size="medium" placeholder="中等尺寸"></vxe-password-input>
+      </vxe-form-item>
+    </vxe-form>
+
+    <vxe-form>
+      <vxe-form-item>
+        <vxe-input v-model="userName" size="small" placeholder="用户名"></vxe-input>
+      </vxe-form-item>
+      <vxe-form-item>
+        <vxe-password-input v-model="val3" size="small" placeholder="小型尺寸"></vxe-password-input>
+      </vxe-form-item>
+    </vxe-form>
+
+    <vxe-form>
+      <vxe-form-item>
+        <vxe-input v-model="userName" size="mini" placeholder="用户名"></vxe-input>
+      </vxe-form-item>
+      <vxe-form-item>
+        <vxe-password-input v-model="val4" size="mini" placeholder="超小尺寸"></vxe-password-input>
+      </vxe-form-item>
+    </vxe-form>
   </div>
 </template>
 
