8010cec85593675a229f82c971118c54df10e656
xuliangzhan <xu_liangzhan@163.com>
2025-12-30 11:28:14 +0800

    update docs

--- other3/src/views/plugin-render-tdesign/install/CodeExample.vue
+++ other3/src/views/plugin-render-tdesign/install/CodeExample.vue
@@ -5,6 +5,9 @@
         <vxe-tip status="primary" title="安装插件 @vxe-ui/plugin-render-tdesign">
           <vxe-link icon="vxe-icon-github-fill" href="https://github.com/x-extends/vxe-ui-plugins/tree/main/install" target="_blank"></vxe-link> 该插件提供了在表格中渲染第三方组件，用于兼容 <vxe-link href="https://www.npmjs.com/package/tdesign-vue" target="_blank">tdesign-vuen</vxe-link> 组件库
         </vxe-tip>
+        <vxe-tip status="success" title="兼容性提示">
+          适配插件用于解决使用第三方组件时无法选中的问题，比如下拉框等无法选中被自动关闭的问题
+        </vxe-tip>
         <vxe-tip status="error">
           对应的第三方组件库 <vxe-link href="https://www.npmjs.com/package/tdesign-vue" target="_blank">tdesign-vue</vxe-link> 需自行去安装，安装完成对应的组件库后就可以在表格中使用了
         </vxe-tip>
