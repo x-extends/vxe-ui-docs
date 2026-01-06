d45599ebccbf928c6c098bb1108cb2f176dad1ce
xuliangzhan <xu_liangzhan@163.com>
2026-01-06 15:07:44 +0800

    update docs

--- v4/src/views/tree/menu/loading/CodeExample.vue
+++ v4/src/views/tree/menu/loading/CodeExample.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <CodeLight path="tree/menu/loading/Demo1">
+    <CodeLight path="tree/menu/loading/Demo1" previewPath="/resource/docsImg/tree_menu_loading.gif">
       <template #tip>
         <vxe-tip status="primary" title="加载中">
           通过 <ApiLink name="tree" prop="menu-config"/>.<ApiLink name="tree" prop="options"/>.<ApiLink name="tree" prop="loading"/> 来配置是否加载中
