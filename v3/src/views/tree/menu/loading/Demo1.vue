e1da5a486aa5f48655113fd61055653e16346770
xuliangzhan <xu_liangzhan@163.com>
2026-01-06 15:02:01 +0800

    update docs

--- v3/src/views/tree/menu/loading/Demo1.vue
+++ v3/src/views/tree/menu/loading/Demo1.vue
@@ -28,41 +28,56 @@ export default Vue.extend({
       menuConfig: {
         options: [
           [
-            { code: '1', name: '新增', loading: true },
-            { code: '2', name: '删除', prefixIcon: 'vxe-icon-delete-fill', loading: false },
+            { code: '1', name: '新增', loading: false, visible: true },
+            { code: '2', name: '删除', prefixIcon: 'vxe-icon-delete-fill', loading: false, visible: true },
             {
               code: '3',
               name: '审批',
-              loading: true,
+              loading: false,
+              visible: true,
               children: [
-                { code: '4', name: '通过', prefixIcon: 'vxe-icon-check', loading: false },
-                { code: '5', name: '不通过', prefixIcon: 'vxe-icon-close', loading: false }
+                { code: '4', name: '通过', prefixIcon: 'vxe-icon-check', loading: false, visible: true },
+                { code: '5', name: '不通过', prefixIcon: 'vxe-icon-close', loading: false, visible: true }
               ]
             },
-            { code: '6', name: '查看', prefixIcon: 'vxe-icon-link', loading: false }
+            { code: '6', name: '查看', prefixIcon: 'vxe-icon-link', loading: false, visible: true }
           ],
           [
             {
               code: '11',
               name: '更多操作',
-              loading: true,
+              loading: false,
+              visible: true,
               children: [
-                { code: '13', name: '编辑', prefixIcon: 'vxe-icon-feedback', loading: false },
+                { code: '13', name: '编辑', prefixIcon: 'vxe-icon-feedback', loading: false, visible: true },
                 { code: '14', name: '取消' }
               ]
             },
-            { code: '10', name: '驳回', prefixIcon: 'vxe-icon-undo', loading: false }
+            { code: '10', name: '驳回', prefixIcon: 'vxe-icon-undo', loading: false, visible: true }
           ]
         ],
         visibleMethod ({ options }) {
+          options.forEach(list => {
+            list.forEach(item => {
+              if (item.code === '1' || item.code === '3') {
+                item.loading = true
+              }
+              if (item.code === '4' || item.code === '11') {
+                item.loading = true
+              }
+            })
+          })
+
           // 模拟后端异步
           setTimeout(() => {
             options.forEach(list => {
               list.forEach(item => {
                 if (item.code === '1' || item.code === '3') {
+                  item.visible = false
                   item.loading = false
                 }
                 if (item.code === '4' || item.code === '11') {
+                  item.visible = false
                   item.loading = false
                 }
               })
