62620191e25193c2fb722e00a34a90111230a77e
xuliangzhan <xu_liangzhan@163.com>
2026-01-13 14:32:55 +0800

    update docs

--- v3/src/views/select/base/remote/CodeExample.vue
+++ v3/src/views/select/base/remote/CodeExample.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <CodeLight path="select/config/remote/Demo1">
+    <CodeLight path="select/base/remote/Demo1">
       <template #tip>
         <vxe-tip status="primary" title="远程搜索">
           启用 <ApiLink name="select" prop="remote"/> 和 <ApiLink name="select" prop="filterable"/> 配合 <ApiLink name="select" prop="remote-config"/>.<ApiLink name="select" prop="queryMethod"/> 自定义调用后端接口处理方法
