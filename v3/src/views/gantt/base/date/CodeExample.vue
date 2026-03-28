fda3b9260dcf2ad4da6409e3cb26fcc79ee95cce
xuliangzhan <xu_liangzhan@163.com>
2026-03-28 11:02:24 +0800

    update docs

--- v3/src/views/gantt/base/date/CodeExample.vue
+++ v3/src/views/gantt/base/date/CodeExample.vue
@@ -3,7 +3,7 @@
     <CodeLight path="gantt/base/date/Demo1">
       <template #tip>
         <vxe-tip status="primary" title="日期格式">
-          支持字符串日期格式自定义，通过 <ApiLink name="gantt" prop="taskViewConfig"/>.<ApiLink name="gantt" prop="taskViewConfig"/> 来设置
+          支持字符串日期格式自定义，通过 <ApiLink name="gantt" prop="task-config"/>.<ApiLink name="gantt" prop="dateFormat"/>='yyyy-MM-dd HH:mm:ss' 来设置
         </vxe-tip>
       </template>
     </CodeLight>
