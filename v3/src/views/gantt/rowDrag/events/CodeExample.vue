29aaa204c7b9990ece2d425dd41079e3affa1b2a
xuliangzhan <xu_liangzhan@163.com>
2026-04-24 10:50:43 +0800

    update docs

--- v3/src/views/gantt/rowDrag/events/CodeExample.vue
+++ v3/src/views/gantt/rowDrag/events/CodeExample.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <CodeLight path="grid/rowDrag/events/Demo1">
+    <CodeLight path="gantt/rowDrag/events/Demo1">
       <template #tip>
         <vxe-tip status="primary" title="行拖拽事件">
-          通过 <ApiLink name="grid" prop="row-dragstart"/>、<ApiLink name="grid" prop="row-dragend"/> 等事件监听拖拽
+          通过 <ApiLink name="gantt" prop="row-dragstart"/>、<ApiLink name="gantt" prop="row-dragend"/> 等事件监听拖拽
         </vxe-tip>
       </template>
     </CodeLight>
