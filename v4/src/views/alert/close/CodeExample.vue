70b7f027a77289991b183728fbcef5bbf81571a0
xuliangzhan <xu_liangzhan@163.com>
2026-05-14 15:11:14 +0800

    update docs

--- v4/src/views/alert/close/CodeExample.vue
+++ v4/src/views/alert/close/CodeExample.vue
@@ -5,5 +5,13 @@
         <vxe-tip status="primary" title="显示关闭按钮"></vxe-tip>
       </template>
     </CodeLight>
+
+    <CodeLight path="alert/close/Demo2">
+      <template #tip>
+        <vxe-tip status="primary">
+          同时显示状态
+        </vxe-tip>
+      </template>
+    </CodeLight>
   </div>
 </template>
