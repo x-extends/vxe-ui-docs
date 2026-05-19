0d005efd1bc12f0205253239dba1284a5ba2b138
xuliangzhan <xu_liangzhan@163.com>
2026-05-19 16:54:53 +0800

    update docs

--- other3/src/views/plugin-export-xlsx/table/export/Demo1.vue
+++ other3/src/views/plugin-export-xlsx/table/export/Demo1.vue
@@ -15,6 +15,8 @@
       <vxe-column field="age" title="Age" :formatter="ageFormatter" :footer-formatter="ageFooterFormatter"></vxe-column>
       <vxe-column field="no1" title="NO1"></vxe-column>
       <vxe-column field="no2" title="NO2" cell-type="string"></vxe-column>
+      <vxe-column field="cardNo1" title="Card No"></vxe-column>
+      <vxe-column field="cardNo2" title="Card No String" cell-type="string"></vxe-column>
     </vxe-table>
   </div>
 </template>
@@ -38,10 +40,10 @@ interface RowVO {
 export default Vue.extend({
   data () {
     const tableData: RowVO[] = [
-      { id: 10001, name: '张三', role: 'Develop', sex: 'Man', age: 28, no1: '028', no2: '028', cardNo1: '6000123456789123', cardNo2: '6000123456789123' },
-      { id: 10002, name: '李四', role: '研发', sex: 'Women', age: 36, no1: '220', no2: '220', cardNo1: '5000123456789123', cardNo2: '5000123456789123' },
-      { id: 10003, name: '王五', role: '产品经理', sex: 'Man', age: 44, no1: '003200', no2: '003200', cardNo1: '7000123456789123', cardNo2: '7000123456789123' },
-      { id: 10004, name: '老六', role: 'Designer', sex: 'Women', age: 38, no1: '02040', no2: '02040', cardNo1: '1000123456789123', cardNo2: '1000123456789123' }
+      { id: 10001, name: '张三', role: 'Develop', sex: 'Man', age: 28, no1: '028', no2: '028', cardNo1: '60001234567', cardNo2: '60001234567' },
+      { id: 10002, name: '李四', role: '研发', sex: 'Women', age: 36, no1: '220', no2: '220', cardNo1: '50001234567', cardNo2: '50001234567' },
+      { id: 10003, name: '王五', role: '产品经理', sex: 'Man', age: 44, no1: '003200', no2: '003200', cardNo1: '70001234567', cardNo2: '70001234567' },
+      { id: 10004, name: '老六', role: 'Designer', sex: 'Women', age: 38, no1: '02040', no2: '02040', cardNo1: '10001234567', cardNo2: '10001234567' }
     ]
 
     const footerData: VxeTablePropTypes.FooterData = [
--- other4/src/views/plugin-export-xlsx/grid/export/Demo1.vue
+++ other4/src/views/plugin-export-xlsx/grid/export/Demo1.vue
@@ -52,10 +52,10 @@ const gridOptions = reactive<VxeGridProps<RowVO>>({
     { field: 'cardNo2', title: 'Card No String', cellType: 'string' }
   ],
   data: [
-    { id: 10001, name: '张三', role: 'Develop', sex: 'Man', age: 28, no1: '028', no2: '028', cardNo1: '6000123456789123', cardNo2: '6000123456789123' },
-    { id: 10002, name: '李四', role: '研发', sex: 'Women', age: 36, no1: '220', no2: '220', cardNo1: '5000123456789123', cardNo2: '5000123456789123' },
-    { id: 10003, name: '王五', role: '产品经理', sex: 'Man', age: 44, no1: '003200', no2: '003200', cardNo1: '7000123456789123', cardNo2: '7000123456789123' },
-    { id: 10004, name: '老六', role: 'Designer', sex: 'Women', age: 38, no1: '02040', no2: '02040', cardNo1: '1000123456789123', cardNo2: '1000123456789123' }
+    { id: 10001, name: '张三', role: 'Develop', sex: 'Man', age: 28, no1: '028', no2: '028', cardNo1: '60001234567', cardNo2: '60001234567' },
+    { id: 10002, name: '李四', role: '研发', sex: 'Women', age: 36, no1: '220', no2: '220', cardNo1: '50001234567', cardNo2: '50001234567' },
+    { id: 10003, name: '王五', role: '产品经理', sex: 'Man', age: 44, no1: '003200', no2: '003200', cardNo1: '70001234567', cardNo2: '70001234567' },
+    { id: 10004, name: '老六', role: 'Designer', sex: 'Women', age: 38, no1: '02040', no2: '02040', cardNo1: '10001234567', cardNo2: '10001234567' }
   ],
   footerData: [
     { seq: '合计', name: '12人', age: '999', no1: '356' }
