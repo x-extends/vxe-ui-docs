aa85dee1cfc3852bb653553b91a29561cd87afaf
xuliangzhan <xu_liangzhan@163.com>
2026-05-19 17:35:19 +0800

    update docs

--- other3/src/views/plugin-export-xlsx/grid/scroll/CodeExample.vue
+++ other3/src/views/plugin-export-xlsx/grid/scroll/CodeExample.vue
@@ -3,7 +3,7 @@
     <CodeLight path="plugin-export-xlsx/grid/scroll/Demo1">
       <template #tip>
         <vxe-tip status="primary" title="大数量导出">
-          当启用虚拟滚动后
+          当启用虚拟滚动后，可以配合 <ApiLink name="grid" prop="export-method"/> 或 <ApiLink name="grid" prop="formatter"/> 来格式化单元格值
         </vxe-tip>
       </template>
     </CodeLight>
--- other3/src/views/plugin-export-xlsx/table/scroll/CodeExample.vue
+++ other3/src/views/plugin-export-xlsx/table/scroll/CodeExample.vue
@@ -3,7 +3,7 @@
     <CodeLight path="plugin-export-xlsx/table/scroll/Demo1">
       <template #tip>
         <vxe-tip status="primary" title="大数量导出">
-          当启用虚拟滚动后
+          当启用虚拟滚动后，可以配合 <ApiLink name="table" prop="export-method"/> 或 <ApiLink name="column" prop="formatter"/> 来格式化单元格值
         </vxe-tip>
       </template>
     </CodeLight>
