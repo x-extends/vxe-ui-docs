393e77c1864a80cd7fa931c6e46507bd4c7316a1
xuliangzhan <xu_liangzhan@163.com>
2026-05-19 22:47:08 +0800

    update docs

--- v4/src/views/color-picker/colors/Demo2.vue
+++ v4/src/views/color-picker/colors/Demo2.vue
@@ -9,6 +9,10 @@ import { ref } from 'vue'
 
 const val1 = ref('')
 const colorList = ref([
-  'rgb(60,179,113)', 'rgb(255,20,147)', 'rgb(255,0,25)', 'rgba(153,50,204,1)', 'rgba(106,90,205,1)'
+  'rgb(60,179,113)',
+  'rgb(255,20,147)',
+  'rgb(255,0,25)',
+  'rgba(153,50,204,1)',
+  'rgba(106,90,205,1)'
 ])
 </script>
