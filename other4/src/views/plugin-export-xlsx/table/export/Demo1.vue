dc664a4f9b17002c62e29d336fdb4a58f53b1cfe
xuliangzhan <xu_liangzhan@163.com>
2026-05-19 16:49:29 +0800

    update docs

--- other4/src/views/plugin-export-xlsx/table/export/Demo1.vue
+++ other4/src/views/plugin-export-xlsx/table/export/Demo1.vue
@@ -32,15 +32,17 @@ interface RowVO {
   age: number
   no1: string
   no2: string
+  cardNo1: string
+  cardNo2: string
 }
 
 const tableRef = ref<VxeTableInstance>()
 
 const tableData = ref<RowVO[]>([
-  { id: 10001, name: '张三', role: 'Develop', sex: 'Man', age: 28, no1: '028', no2: '028' },
-  { id: 10002, name: '李四', role: '研发', sex: 'Women', age: 36, no1: '220', no2: '220' },
-  { id: 10003, name: '王五', role: '产品经理', sex: 'Man', age: 44, no1: '003200', no2: '003200' },
-  { id: 10004, name: '老六', role: 'Designer', sex: 'Women', age: 38, no1: '02040', no2: '02040' }
+  { id: 10001, name: '张三', role: 'Develop', sex: 'Man', age: 28, no1: '028', no2: '028', cardNo1: '6000123456789123', cardNo2: '6000123456789123' },
+  { id: 10002, name: '李四', role: '研发', sex: 'Women', age: 36, no1: '220', no2: '220', cardNo1: '5000123456789123', cardNo2: '5000123456789123' },
+  { id: 10003, name: '王五', role: '产品经理', sex: 'Man', age: 44, no1: '003200', no2: '003200', cardNo1: '7000123456789123', cardNo2: '7000123456789123' },
+  { id: 10004, name: '老六', role: 'Designer', sex: 'Women', age: 38, no1: '02040', no2: '02040', cardNo1: '1000123456789123', cardNo2: '1000123456789123' }
 ])
 
 const footerData = ref<VxeTablePropTypes.FooterData>([
