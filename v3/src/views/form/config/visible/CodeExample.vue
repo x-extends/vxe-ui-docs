b02072da45fc8cb7ea2d83e33732d85876a9ef57
xuliangzhan <xu_liangzhan@163.com>
2026-06-03 17:09:13 +0800

    update docs

--- v3/src/views/form/config/visible/CodeExample.vue
+++ v3/src/views/form/config/visible/CodeExample.vue
@@ -3,7 +3,7 @@
     <CodeLight path="form/config/visible/Demo1">
       <template #tip>
         <vxe-tip status="primary" title="显示与隐藏项">
-          通过调用 <ApiLink name="form-item" prop="showItem"/>, <ApiLink name="form-item" prop="hideItem"/> 方法来设置是否显示
+          通过调用 <ApiLink name="form" prop="showItem"/>, <ApiLink name="form" prop="hideItem"/> 方法来设置是否显示
         </vxe-tip>
       </template>
     </CodeLight>
--- v4/src/views/form/basics/visible/CodeExample.vue
+++ v4/src/views/form/basics/visible/CodeExample.vue
@@ -2,9 +2,9 @@
   <div>
     <CodeLight path="form/basics/visible/Demo1">
       <template #tip>
-              <vxe-tip status="primary" title="显示与隐藏项">
-                通过调用 <ApiLink name="form-item" prop="showItem"/>, <ApiLink name="form-item" prop="hideItem"/> 方法来设置是否显示
-              </vxe-tip>
+        <vxe-tip status="primary" title="显示与隐藏项">
+          通过调用 <ApiLink name="form" prop="showItem"/>, <ApiLink name="form" prop="hideItem"/> 方法来设置是否显示
+        </vxe-tip>
       </template>
     </CodeLight>
   </div>
