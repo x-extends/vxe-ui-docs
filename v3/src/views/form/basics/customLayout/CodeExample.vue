cfff8f097a7cd4be0ef7fa12fcf9fe2decfdb4ad
xuliangzhan <xu_liangzhan@163.com>
2026-06-11 21:25:19 +0800

    update docs

--- v3/src/views/form/basics/customLayout/CodeExample.vue
+++ v3/src/views/form/basics/customLayout/CodeExample.vue
@@ -3,7 +3,7 @@
     <CodeLight path="form/basics/customLayout/Demo1">
       <template #tip>
         <vxe-tip status="primary" title="自定义布局">
-          通过设置 <ApiLink name="form" prop="custom-layout"/> 启用自定义布局功能
+          默认是配置式布局，当需要试用灵活的自定义布局时，可以通过设置 <ApiLink name="form" prop="custom-layout"/> 启用，启用后可以使用任意元素组合布局
         </vxe-tip>
       </template>
     </CodeLight>
