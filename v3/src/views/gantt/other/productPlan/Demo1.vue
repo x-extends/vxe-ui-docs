32b2930ff1d2e87f52eabac425a441b1436ff8ab
xuliangzhan <xu_liangzhan@163.com>
2026-06-13 16:29:43 +0800

    update docs

--- v3/src/views/gantt/other/productPlan/Demo1.vue
+++ v3/src/views/gantt/other/productPlan/Demo1.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
     <vxe-gantt v-bind="ganttOptions">
+      <template #user_default="{ row }">
+        <div>处理人：{{ row.createBy }}</div>
+        <div>开始时间：{{ row.start }}</div>
+        <div>结束时间：{{ row.end }}</div>
+      </template>
+
       <template #task-bar="{ row }">
         <div class="custom-task-bar" :style="{ backgroundColor: row.bgColor }">
           <div class="custom-task-bar-img">
@@ -34,11 +40,13 @@ import { VxeGanttProps } from 'vxe-gantt'
 interface RowVO {
   id: number
   title: string
+  productImg: string
   start: string
   end: string
   progress: number
   imgUrl: string
   bgColor: string
+  createBy: string
 }
 
 export default Vue.extend({
@@ -97,18 +105,29 @@ export default Vue.extend({
         }
       },
       columns: [
-        { field: 'title', title: '任务名称' },
-        { field: 'start', title: '开始时间', width: 100 },
-        { field: 'end', title: '结束时间', width: 100 }
+        { field: 'title', title: '任务名称', width: 140 },
+        {
+          field: 'productImg',
+          title: '图片',
+          width: 100,
+          cellRender: {
+            name: 'VxeImage',
+            props: {
+              width: 80,
+              height: 80
+            }
+          }
+        },
+        { field: 'createBy', title: '负责人', width: 200, slots: { default: 'user_default' } }
       ],
       data: [
-        { id: 10002, title: '城市道路修理进度', start: '2024-03-03', end: '2024-03-10', progress: 20, bgColor: '#c1c452', imgUrl: 'https://vxeui.com/resource/productImg/product9.png' },
-        { id: 10004, title: '超级大工程', start: '2024-03-05', end: '2024-03-12', progress: 15, bgColor: '#fd9393', imgUrl: 'https://vxeui.com/resource/productImg/product8.png' },
-        { id: 10006, title: '一个小目标项目', start: '2024-03-10', end: '2024-03-21', progress: 25, bgColor: '#92c1f1', imgUrl: 'https://vxeui.com/resource/productImg/product1.png' },
-        { id: 10007, title: '某某计划', start: '2024-03-15', end: '2024-03-24', progress: 70, bgColor: '#fad06c', imgUrl: 'https://vxeui.com/resource/productImg/product3.png' },
-        { id: 10008, title: '某某科技项目', start: '2024-03-20', end: '2024-04-05', progress: 50, bgColor: '#e78dd2', imgUrl: 'https://vxeui.com/resource/productImg/product11.png' },
-        { id: 10009, title: '地铁建设工程', start: '2024-03-22', end: '2024-03-29', progress: 50, bgColor: '#8be1e6', imgUrl: 'https://vxeui.com/resource/productImg/product7.png' },
-        { id: 10010, title: '蓝天计划', start: '2024-03-28', end: '2024-04-04', progress: 50, bgColor: '#78e6d1', imgUrl: 'https://vxeui.com/resource/productImg/product5.png' }
+        { id: 10002, title: '城市道路修理进度', start: '2024-03-03', end: '2024-03-10', progress: 20, createBy: '小王', bgColor: '#c1c452', productImg: 'https://vxeui.com/resource/img/bq475.gif', imgUrl: 'https://vxeui.com/resource/productImg/product9.png' },
+        { id: 10004, title: '超级大工程', start: '2024-03-05', end: '2024-03-12', progress: 15, createBy: '张三', bgColor: '#fd9393', productImg: 'https://vxeui.com/resource/img/673.gif', imgUrl: 'https://vxeui.com/resource/productImg/product8.png' },
+        { id: 10006, title: '一个小目标项目', start: '2024-03-10', end: '2024-03-21', progress: 25, createBy: '老六', bgColor: '#92c1f1', productImg: 'https://vxeui.com/resource/img/546.gif', imgUrl: 'https://vxeui.com/resource/productImg/product1.png' },
+        { id: 10007, title: '某某计划', start: '2024-03-15', end: '2024-03-24', progress: 70, createBy: '李四', bgColor: '#fad06c', productImg: 'https://vxeui.com/resource/img/bq987.gif', imgUrl: 'https://vxeui.com/resource/productImg/product3.png' },
+        { id: 10008, title: '某某科技项目', start: '2024-03-20', end: '2024-04-05', progress: 50, createBy: '小东', bgColor: '#e78dd2', productImg: 'https://vxeui.com/resource/img/bq673.gif', imgUrl: 'https://vxeui.com/resource/productImg/product11.png' },
+        { id: 10009, title: '地铁建设工程', start: '2024-03-22', end: '2024-03-29', progress: 50, createBy: '王五', bgColor: '#8be1e6', productImg: 'https://vxeui.com/resource/img/673.gif', imgUrl: 'https://vxeui.com/resource/productImg/product7.png' },
+        { id: 10010, title: '蓝天计划', start: '2024-03-28', end: '2024-04-04', progress: 50, createBy: '小明', bgColor: '#78e6d1', productImg: 'https://vxeui.com/resource/img/bq546.gif', imgUrl: 'https://vxeui.com/resource/productImg/product5.png' }
       ]
     }
 
