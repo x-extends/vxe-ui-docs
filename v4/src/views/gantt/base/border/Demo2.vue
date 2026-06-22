312b870428029b89fe2eec32c047d2c1514c49a6
xuliangzhan <xu_liangzhan@163.com>
2026-06-22 10:07:03 +0800

    update docs

--- v4/src/views/gantt/base/border/Demo2.vue
+++ v4/src/views/gantt/base/border/Demo2.vue
@@ -31,7 +31,7 @@ const ganttOptions = reactive<VxeGanttProps<RowVO>>({
     { field: 'end', title: '结束时间', width: 100 }
   ],
   data: [
-    { id: 10002, name: '城市道路修理进度', start: '2024-03-03', end: '2024-03-08', progress: 10 },
+    { id: 10002, name: '城市道路修理进度', start: '2024-03-03', end: '2024-03-03', progress: 10 },
     { id: 10004, name: '超级大工程', start: '2024-03-05', end: '2024-03-11', progress: 15 },
     { id: 10006, name: '一个小目标项目', start: '2024-03-10', end: '2024-03-21', progress: 5 },
     { id: 10007, name: '某某计划', start: '2024-03-15', end: '2024-03-24', progress: 70 },
