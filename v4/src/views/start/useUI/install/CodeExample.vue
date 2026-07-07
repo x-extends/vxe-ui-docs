024e0054ef1c48e305cae38dec16bf4fbaaf997f
xuliangzhan <xu_liangzhan@163.com>
2026-07-08 01:25:03 +0800

    update docs

--- v4/src/views/start/useUI/install/CodeExample.vue
+++ v4/src/views/start/useUI/install/CodeExample.vue
@@ -33,9 +33,11 @@
             // ...
             import VxeUIBase from 'vxe-pc-ui'
             import 'vxe-pc-ui/es/style.css'
+            import VxeUITable from 'vxe-table'
+            import 'vxe-table/es/style.css'
             // ...
 
-            createApp(App).use(VxeUIBase).mount('#app')
+            createApp(App).use(VxeUIBase).use(VxeUITable).mount('#app')
             // ...">
           </pre-code>
         </pre>
