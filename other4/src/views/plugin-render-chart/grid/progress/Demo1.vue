39ca320be75c5d7c61f4fd9a8da36b97049a1906
xuliangzhan <xu_liangzhan@163.com>
2026-07-14 15:43:48 +0800

    update docs

--- other4/src/views/plugin-render-chart/grid/progress/Demo1.vue
+++ other4/src/views/plugin-render-chart/grid/progress/Demo1.vue
@@ -13,6 +13,7 @@ interface RowVO {
   name: string
   num41: number
   num42: number
+  num43: number
 }
 
 const num41CellRender = reactive<VxeColumnPropTypes.CellRender>({
@@ -31,6 +32,21 @@ const num42CellRender = reactive<VxeColumnPropTypes.CellRender>({
   }
 })
 
+const num43CellRender = reactive<VxeColumnPropTypes.CellRender>({
+  name: 'progress',
+  props: {
+    completedBgColor ({ row }) {
+      if (row.num43 > 70) {
+        return '9bd1b8'
+      }
+      if (row.num43 > 30) {
+        return '#ffe566'
+      }
+      return '#cc3f3b'
+    }
+  }
+})
+
 const gridOptions = reactive<VxeGridProps<RowVO>>({
   border: true,
   showOverflow: true,
@@ -42,19 +58,20 @@ const gridOptions = reactive<VxeGridProps<RowVO>>({
     { type: 'seq', width: 70 },
     { field: 'name', title: 'Name' },
     { field: 'num41', title: '进度条100', width: 180, cellRender: num41CellRender },
-    { field: 'num42', title: '进度条10', width: 180, cellRender: num42CellRender }
+    { field: 'num42', title: '进度条10', width: 180, cellRender: num42CellRender },
+    { field: 'num43', title: '进度条多颜色', width: 180, cellRender: num43CellRender }
   ],
   data: [
-    { id: 101, name: 'test1', num41: 10, num42: 8 },
-    { id: 102, name: 'test2', num41: 30, num42: 0 },
-    { id: 103, name: 'test3', num41: 71, num42: 1 },
-    { id: 104, name: 'test4', num41: 97, num42: 4 },
-    { id: 105, name: 'test5', num41: 100, num42: 3 },
-    { id: 106, name: 'test6', num41: 85, num42: 9 },
-    { id: 107, name: 'test7', num41: 0, num42: 10 },
-    { id: 108, name: 'test8', num41: 23, num42: 4 },
-    { id: 109, name: 'test9', num41: 33, num42: 7 },
-    { id: 1010, name: 'test10', num41: 66, num42: 2 }
+    { id: 101, name: 'test1', num41: 10, num42: 8, num43: 39 },
+    { id: 102, name: 'test2', num41: 30, num42: 0, num43: 18 },
+    { id: 103, name: 'test3', num41: 71, num42: 1, num43: 40 },
+    { id: 104, name: 'test4', num41: 97, num42: 4, num43: 10 },
+    { id: 105, name: 'test5', num41: 100, num42: 3, num43: 0 },
+    { id: 106, name: 'test6', num41: 85, num42: 9, num43: 55 },
+    { id: 107, name: 'test7', num41: 0, num42: 10, num43: 44 },
+    { id: 108, name: 'test8', num41: 23, num42: 4, num43: 100 },
+    { id: 109, name: 'test9', num41: 33, num42: 7, num43: 90 },
+    { id: 1010, name: 'test10', num41: 66, num42: 2, num43: 88 }
   ]
 })
 </script>
--- other4/src/views/plugin-render-chart/table/progress/Demo1.vue
+++ other4/src/views/plugin-render-chart/table/progress/Demo1.vue
@@ -10,6 +10,7 @@
       <vxe-column field="name" title="Name"></vxe-column>
       <vxe-column field="num41" title="进度条100" width="180" :cell-render="num41CellRender"></vxe-column>
       <vxe-column field="num42" title="进度条10" width="180" :cell-render="num42CellRender"></vxe-column>
+      <vxe-column field="num43" title="进度条多颜色" width="180" :cell-render="num43CellRender"></vxe-column>
     </vxe-table>
   </div>
 </template>
@@ -23,19 +24,20 @@ interface RowVO {
   name: string
   num41: number
   num42: number
+  num43: number
 }
 
 const tableData = ref<RowVO[]>([
-  { id: 101, name: 'test1', num41: 10, num42: 8 },
-  { id: 102, name: 'test2', num41: 30, num42: 0 },
-  { id: 103, name: 'test3', num41: 71, num42: 1 },
-  { id: 104, name: 'test4', num41: 97, num42: 4 },
-  { id: 105, name: 'test5', num41: 100, num42: 3 },
-  { id: 106, name: 'test6', num41: 85, num42: 9 },
-  { id: 107, name: 'test7', num41: 0, num42: 10 },
-  { id: 108, name: 'test8', num41: 23, num42: 4 },
-  { id: 109, name: 'test9', num41: 33, num42: 7 },
-  { id: 1010, name: 'test10', num41: 66, num42: 2 }
+  { id: 101, name: 'test1', num41: 10, num42: 8, num43: 39 },
+  { id: 102, name: 'test2', num41: 30, num42: 0, num43: 18 },
+  { id: 103, name: 'test3', num41: 71, num42: 1, num43: 40 },
+  { id: 104, name: 'test4', num41: 97, num42: 4, num43: 10 },
+  { id: 105, name: 'test5', num41: 100, num42: 3, num43: 0 },
+  { id: 106, name: 'test6', num41: 85, num42: 9, num43: 55 },
+  { id: 107, name: 'test7', num41: 0, num42: 10, num43: 44 },
+  { id: 108, name: 'test8', num41: 23, num42: 4, num43: 100 },
+  { id: 109, name: 'test9', num41: 33, num42: 7, num43: 90 },
+  { id: 1010, name: 'test10', num41: 66, num42: 2, num43: 88 }
 ])
 
 const num41CellRender = reactive<VxeColumnPropTypes.CellRender>({
@@ -50,6 +52,21 @@ const num42CellRender = reactive<VxeColumnPropTypes.CellRender>({
     completedBgColor: '#D48265',
     label: {
       formatter: '{{value}}个'
+    }
+  }
+})
+
+const num43CellRender = reactive<VxeColumnPropTypes.CellRender>({
+  name: 'progress',
+  props: {
+    completedBgColor ({ row }) {
+      if (row.num43 > 70) {
+        return '9bd1b8'
+      }
+      if (row.num43 > 30) {
+        return '#ffe566'
+      }
+      return '#cc3f3b'
     }
   }
 })
