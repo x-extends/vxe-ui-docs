53bfc1a9fbfc6021074fb575e8435ab44e8e7bd3
xuliangzhan <xu_liangzhan@163.com>
2026-07-14 16:19:23 +0800

    update docs

--- other3/src/views/plugin-render-chart/table/progress/Demo1.vue
+++ other3/src/views/plugin-render-chart/table/progress/Demo1.vue
@@ -31,14 +31,14 @@ export default Vue.extend({
   data () {
     const tableData: RowVO[] = [
       { id: 101, name: 'test1', num41: 60, num42: 8, num43: 80 },
-      { id: 102, name: 'test2', num41: 30, num42: 5, num43: 70 },
+      { id: 102, name: 'test2', num41: 30, num42: 5, num43: 50 },
       { id: 103, name: 'test3', num41: 71, num42: 9, num43: 60 },
-      { id: 104, name: 'test4', num41: 97, num42: 4, num43: 40 },
-      { id: 105, name: 'test5', num41: 100, num42: 4, num43: 50 },
+      { id: 104, name: 'test4', num41: 97, num42: 4, num43: 100 },
+      { id: 105, name: 'test5', num41: 100, num42: 4, num43: 70 },
       { id: 106, name: 'test6', num41: 85, num42: 9, num43: 55 },
       { id: 107, name: 'test7', num41: 50, num42: 10, num43: 60 },
       { id: 108, name: 'test8', num41: 73, num42: 4, num43: 100 },
-      { id: 109, name: 'test9', num41: 43, num42: 7, num43: 90 },
+      { id: 109, name: 'test9', num41: 43, num42: 7, num43: 40 },
       { id: 1010, name: 'test10', num41: 66, num42: 6, num43: 88 }
     ]
 
