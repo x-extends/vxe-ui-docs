52dfb4b2a9cfdef663ddc9341f11bd41835b54d6
xuliangzhan <xu_liangzhan@163.com>
2026-08-18 16:34:30 +0800

    update docs

--- v4/eslint.config.js
+++ v4/eslint.config.js
@@ -21,6 +21,9 @@ export default defineConfig([
       }
     }
   },
+  {
+    ignores: ['**/node_modules/', '.git/']
+  },
   tseslint.configs.recommended,
   pluginVue.configs['flat/essential'],
   {
@@ -63,7 +66,7 @@ export default defineConfig([
       'no-debugger': 'error',
       'vue/no-v-model-argument': 'off',
       'vue/multi-word-component-names': 'off',
-      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': 'warn'
     }
   }
