23282d57e9f21f5d9a70b22a797acd88d5fbbfa9
xuliangzhan <xu_liangzhan@163.com>
2026-08-20 11:59:37 +0800

    update docs

--- v3/src/views/tag/size/Demo1.vue
+++ v3/src/views/tag/size/Demo1.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <vxe-tag size="xlarge" content="特大尺寸"></vxe-tag>
-    <vxe-tag size="large" content="大型尺寸"></vxe-tag>
+    <vxe-tag status="primary" size="xlarge" content="特大尺寸"></vxe-tag>
+    <vxe-tag status="primary" size="large" content="大型尺寸"></vxe-tag>
     <vxe-tag status="primary" content="默认尺寸"></vxe-tag>
     <vxe-tag status="primary" size="medium" content="中等尺寸"></vxe-tag>
     <vxe-tag status="primary" size="small" content="小型尺寸"></vxe-tag>
