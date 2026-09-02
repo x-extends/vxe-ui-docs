475bef70ee68daf80c583724e3035a39ce9e78e1
xuliangzhan <xu_liangzhan@163.com>
2026-09-02 18:15:26 +0800

    update docs

--- v3/src/views/modal/modal/within/CodeExample.vue
+++ v3/src/views/modal/modal/within/CodeExample.vue
@@ -6,6 +6,7 @@
           通过设置 <ApiLink name="modal" prop="append-to" /> 和 <ApiLink name="modal" prop="is-within-append-to" /> 来将弹窗挂载到指定区域显示
         </vxe-tip>
         <vxe-tip status="error" title="小提示">
+          <div> 当使用 append-to 时，必须要保证在该组件渲染之前存在该元素</div>
           <div> 父元素（挂载元素）需要有定位属性，如：position: relative</div>
         </vxe-tip>
       </template>
