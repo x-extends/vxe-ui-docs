a7f442b00359afd9359f342d18fbc1ba99839bc8
xuliangzhan <xu_liangzhan@163.com>
2026-09-06 19:33:46 +0800

    update docs

--- v4/src/views/icon/base/CodeExample.vue
+++ v4/src/views/icon/base/CodeExample.vue
@@ -9,17 +9,17 @@
     </CodeLight>
 
     <div>
-      <vxe-card class="icon-group-card" v-for="(group, gIndex) in groupList" :key="gIndex" :title="group.name" width="100%">
+      <vxe-card v-for="(group, gIndex) in groupList" :key="gIndex" class="icon-group-card" :title="group.name" width="100%">
         <ul class="icon-group-list">
-          <li class="icon-item" v-for="name in group.children" :key="name">
-            <div class="icon-preview"  @click="copyEvent(`${prefixClass}${name}`)" title="点击复制内容">
+          <li v-for="name in group.children" :key="name" class="icon-item">
+            <div class="icon-preview" title="点击复制内容" @click="copyEvent(`${prefixClass}${name}`)">
               <vxe-icon :name="name"></vxe-icon>
             </div>
             <div class="icon-title">
-              <vxe-button mode="text" status="info" size="mini" @click="copyEvent(name)" title="点击复制内容">{{ name }}</vxe-button>
+              <vxe-button mode="text" status="info" size="mini" title="点击复制内容" @click="copyEvent(name)">{{ name }}</vxe-button>
             </div>
             <div class="icon-name">
-              <vxe-button mode="text" status="info" size="mini" @click="copyEvent(`${prefixClass}${name}`)" title="点击复制内容">{{ prefixClass }}{{ name }}</vxe-button>
+              <vxe-button mode="text" status="info" size="mini" title="点击复制内容" @click="copyEvent(`${prefixClass}${name}`)">{{ prefixClass }}{{ name }}</vxe-button>
             </div>
           </li>
         </ul>
@@ -33,7 +33,9 @@ import { ref } from 'vue'
 import { VxeUI, VxeIconPropTypes } from 'vxe-pc-ui'
 
 const prefixClass = ref('vxe-icon-')
-const iconList: VxeIconPropTypes.Name[] = ['add', 'add-sub', 'add-user', 'add-users', 'align-center', 'align-left', 'align-right', 'alipay', 'arrow-double-left', 'arrow-double-right', 'arrow-down', 'arrow-left', 'arrow-right', 'arrow-up', 'arrows-down', 'arrows-left', 'arrows-right', 'arrows-up', 'association-form', 'bell', 'bell-fill', 'bottom', 'calendar', 'caret-down', 'caret-left', 'caret-right', 'caret-up', 'carousel', 'chart-bar-x', 'chart-bar-y', 'chart-gantt', 'chart-line', 'chart-pie', 'chart-radar', 'chat', 'chat-fill', 'check', 'checkbox-checked', 'checkbox-checked-fill', 'checkbox-indeterminate-fill', 'checkbox-unchecked', 'checkbox-unchecked-fill', 'click-button', 'close', 'cloud-download', 'cloud-upload', 'cloudy', 'code', 'comment', 'company', 'copy', 'custom-column', 'cut', 'delete', 'delete-fill', 'doc-search', 'dot', 'download', 'drag-handle', 'dropper', 'edit', 'ellipsis-h', 'ellipsis-v', 'empty', 'end-page', 'envelope', 'envelope-fill', 'envelope-open', 'envelope-open-fill', 'error-circle', 'error-circle-fill', 'exclamation', 'eye-fill', 'eye-fill-close', 'feedback', 'file', 'file-excel', 'file-image', 'file-markdown', 'file-pdf', 'file-ppt', 'file-txt', 'file-word', 'file-zip', 'fixed-left', 'fixed-left-fill', 'fixed-right', 'fixed-right-fill', 'flag', 'flag-fill', 'flow-branch', 'folder', 'folder-open', 'form', 'fullscreen', 'funnel', 'funnel-clear', 'gitee-fill', 'github-fill', 'goods', 'goods-fill', 'grouping', 'heavy-rain', 'home', 'home-fill', 'home-page', 'indicator', 'info-circle', 'info-circle-fill', 'information', 'input', 'language-switch', 'layout', 'lightning', 'link', 'location', 'location-fill', 'lock', 'lock-fill', 'maximize', 'menu', 'menu-fold', 'menu-unfold', 'merge-cells', 'message-fill', 'microphone', 'microphone-fill', 'minimize', 'minus', 'mobile', 'modal', 'moon', 'more', 'more-fill', 'no-drop', 'notice-left-fill', 'notice-right-fill', 'num-list', 'number', 'paste', 'pc', 'pct-1-1', 'pct-full', 'picture', 'picture-fill', 'platform', 'print', 'print-batch', 'question', 'question-circle', 'question-circle-fill', 'radio-checked', 'radio-checked-fill', 'radio-unchecked', 'radio-unchecked-fill', 'recover', 'refresh', 'repeat', 'rich-text', 'rmb', 'rotate-left', 'rotate-right', 'row-col', 'save', 'search', 'search-zoom-in', 'search-zoom-out', 'select', 'send', 'send-fill', 'service', 'setting', 'setting-fill', 'share', 'share-fill', 'signature', 'slider', 'sort', 'sort-alpha-asc', 'sort-alpha-desc', 'sort-asc', 'sort-desc', 'sort-numeric-asc', 'sort-numeric-desc', 'spinner', 'square', 'square-caret-right', 'square-caret-right-fill', 'square-checked', 'square-checked-fill', 'square-close', 'square-close-fill', 'square-down', 'square-down-fill', 'square-fill', 'square-left', 'square-left-fill', 'square-minus', 'square-minus-fill', 'square-plus', 'square-plus-fill', 'square-plus-square', 'square-right', 'square-right-fill', 'square-square', 'square-up', 'square-up-fill', 'star', 'star-fill', 'star-half', 'style', 'subtable', 'success-circle', 'success-circle-fill', 'sunny', 'swap', 'swap-down', 'swap-left', 'swap-right', 'swap-up', 'switch', 'table', 'tabs', 'tag', 'text', 'textarea', 'time', 'tools', 'top', 'tree-select', 'undo', 'unlock', 'unlock-fill', 'upload', 'usd', 'user', 'user-fill', 'values', 'voice', 'voice-fill', 'warning-circle', 'warning-circle-fill', 'warning-triangle', 'warning-triangle-fill', 'wechat', 'zoom-in', 'zoom-out']
+const iconList: VxeIconPropTypes.Name[] = [
+  'add', 'add-sub', 'add-user', 'add-users', 'align-center', 'align-left', 'align-right', 'alipay', 'arrow-double-left', 'arrow-double-right', 'arrow-down', 'arrow-left', 'arrow-right', 'arrow-up', 'arrows-down', 'arrows-left', 'arrows-right', 'arrows-up', 'association-form', 'bell', 'bell-fill', 'bottom', 'calendar', 'caret-down', 'caret-left', 'caret-right', 'caret-up', 'carousel', 'chart-bar-x', 'chart-bar-y', 'chart-gantt', 'chart-line', 'chart-pie', 'chart-radar', 'chat', 'chat-fill', 'check', 'checkbox-checked', 'checkbox-checked-fill', 'checkbox-indeterminate-fill', 'checkbox-unchecked', 'checkbox-unchecked-fill', 'click-button', 'close', 'cloud-download', 'cloud-upload', 'cloudy', 'code', 'comment', 'company', 'copy', 'custom-column', 'cut', 'delete', 'delete-fill', 'doc-search', 'dot', 'download', 'drag-handle', 'dropper', 'edit', 'ellipsis-h', 'ellipsis-v', 'empty', 'end-page', 'envelope', 'envelope-fill', 'envelope-open', 'envelope-open-fill', 'error-circle', 'error-circle-fill', 'exclamation', 'eye-fill', 'eye-fill-close', 'feedback', 'file', 'file-excel', 'file-image', 'file-markdown', 'file-pdf', 'file-ppt', 'file-txt', 'file-word', 'file-zip', 'fixed-left', 'fixed-left-fill', 'fixed-right', 'fixed-right-fill', 'flag', 'flag-fill', 'flow-branch', 'folder', 'folder-open', 'form', 'fullscreen', 'funnel', 'funnel-clear', 'gitee-fill', 'github-fill', 'goods', 'goods-fill', 'grouping', 'heavy-rain', 'home', 'home-fill', 'home-page', 'indicator', 'info-circle', 'info-circle-fill', 'information', 'input', 'language-switch', 'layout', 'lightning', 'link', 'location', 'location-fill', 'lock', 'lock-fill', 'maximize', 'menu', 'menu-fold', 'menu-unfold', 'merge-cells', 'message-fill', 'microphone', 'microphone-fill', 'minimize', 'minus', 'mobile', 'modal', 'moon', 'more', 'more-fill', 'no-drop', 'notice-left-fill', 'notice-right-fill', 'num-list', 'number', 'paste', 'pause', 'pc', 'pct-1-1', 'pct-full', 'picture', 'picture-fill', 'platform', 'play', 'print', 'print-batch', 'question', 'question-circle', 'question-circle-fill', 'radio-checked', 'radio-checked-fill', 'radio-unchecked', 'radio-unchecked-fill', 'recover', 'refresh', 'repeat', 'rich-text', 'rmb', 'rotate-left', 'rotate-right', 'row-col', 'save', 'search', 'search-zoom-in', 'search-zoom-out', 'select', 'send', 'send-fill', 'service', 'setting', 'setting-fill', 'share', 'share-fill', 'signature', 'slider', 'sort', 'sort-alpha-asc', 'sort-alpha-desc', 'sort-asc', 'sort-desc', 'sort-numeric-asc', 'sort-numeric-desc', 'spinner', 'square', 'square-caret-right', 'square-caret-right-fill', 'square-checked', 'square-checked-fill', 'square-close', 'square-close-fill', 'square-down', 'square-down-fill', 'square-fill', 'square-left', 'square-left-fill', 'square-minus', 'square-minus-fill', 'square-plus', 'square-plus-fill', 'square-plus-square', 'square-right', 'square-right-fill', 'square-square', 'square-up', 'square-up-fill', 'star', 'star-fill', 'star-half', 'stop', 'style', 'subtable', 'success-circle', 'success-circle-fill', 'sunny', 'swap', 'swap-down', 'swap-left', 'swap-right', 'swap-up', 'switch', 'table', 'tabs', 'tag', 'text', 'textarea', 'time', 'tools', 'top', 'tree-select', 'undo', 'unlock', 'unlock-fill', 'upload', 'usd', 'user', 'user-fill', 'values', 'voice', 'voice-fill', 'warning-circle', 'warning-circle-fill', 'warning-triangle', 'warning-triangle-fill', 'wechat', 'zoom-in', 'zoom-out'
+]
 
 const groupList: { name: string, children: VxeIconPropTypes.Name[] }[] = [
   {
@@ -127,6 +129,12 @@ const groupList: { name: string, children: VxeIconPropTypes.Name[] }[] = [
       'square-down-fill', 'square-down', 'square-left-fill', 'square-left', 'square-up-fill', 'square-up', 'square-right-fill', 'square-right',
       'square-caret-right-fill', 'square-caret-right', 'square-fill', 'square',
       'square-plus-square'
+    ]
+  },
+  {
+    name: '播放',
+    children: [
+      'play', 'pause', 'stop'
     ]
   },
   {
