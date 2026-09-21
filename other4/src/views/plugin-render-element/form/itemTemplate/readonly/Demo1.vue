3dbf7a411a7089cc301334332c561091ac20c267
xuliangzhan <xu_liangzhan@163.com>
2026-09-21 22:39:38 +0800

    update docs

--- other4/src/views/plugin-render-element/form/itemTemplate/readonly/Demo1.vue
+++ other4/src/views/plugin-render-element/form/itemTemplate/readonly/Demo1.vue
@@ -14,13 +14,13 @@
         <vxe-form-item title="名称" field="name" span="12" :item-render="{}">
           <template #default="{ readonly }">
             <span v-if="readonly">查看模式：{{ formData.name }}</span>
-            <el-input v-model="formData.name"></el-input>
+            <el-input v-else v-model="formData.name"></el-input>
           </template>
         </vxe-form-item>
         <vxe-form-item title="数字" field="num" span="8" :item-render="{}">
           <template #default="{ readonly }">
             <span v-if="readonly">查看模式：{{ formData.num }}</span>
-            <el-input v-model="formData.num"></el-input>
+            <el-input v-else v-model="formData.num"></el-input>
           </template>
         </vxe-form-item>
       </vxe-form-group>
